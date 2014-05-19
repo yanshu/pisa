80b3e1a454346853972b70896edb5846f37dbf79
Sebastian Boeser <sboeser@physik.uni-bonn.de>
2014-05-19 09:16:09 +0200

    Add all subdirs to PATH

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 echo "Setting up PISA..."
 export PISA=$( cd "$( dirname "$0" )" && pwd )
-export PYTHONPATH=$PYTHONPATH:$PISA
-export PATH=$PATH:$PISA/flux
+export PYTHONPATH=$PISA:$PYTHONPATH
+export PATH=$PISA/flux:$PISA/trigger:$PISA/oscillations:$PISA/reco:$PISA/pid:$PATH
