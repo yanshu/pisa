2e6adbf0686c81c5540123c986b457fd42ed93f9
Sebastian Boeser <sboeser@physik.uni-bonn.de>
2014-05-26 20:50:17 +0200

    First attempt at releasable build

---
+++ setup.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#
+# Allow PISA to be distributed via distutils, i.e the user can just
+#
+#   pip install git+http://git@github.com/sboeser/pisa/master
+#
+# author: Sebastian Boeser
+#         sboeser@physik.uni-bonn.de
+
+from distutils.core import setup, Extension
+from distutils.command import install_headers
+
+setup(
+  name='pisa',
+  version='0.1',
+  description='PINGU Simulation and Analysis',
+  author='Sebastian Boeser',
+  author_email='sboeser@physik.uni-bonn.de',
+  url='http://github.com/sboeser/pisa',
+  packages=['pisa','pisa.flux','pisa.oscillations','pisa.oscillations.prob3'],
+  ext_package='pisa.oscillations.prob3',
+  ext_modules=[Extension('_BargerPropagator',
+                   ['pisa/oscillations/prob3/BargerPropagator.i',
+                    'pisa/oscillations/prob3/BargerPropagator.cc',
+                    'pisa/oscillations/prob3/EarthDensity.cc',
+                    'pisa/oscillations/prob3/mosc.c',
+                    'pisa/oscillations/prob3/mosc3.c'],
+                    swig_opts=['-c++'])]
+)
