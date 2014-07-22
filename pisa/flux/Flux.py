882b6399285d92a42e8f148a8d1520fbcfd0f55b
schultel <lschulte@physik.uni-bonn.de>
2014-07-22 19:14:40 +0200

    Set default flux to South Pole at solar maximum

--- pisa/flux/Flux.py
+++ pisa/flux/Flux.py
@@ -71,7 +71,7 @@ if __name__ == '__main__':
     
     parser.add_argument('--flux_file', metavar='FILE', type=str,
         help= '''Input flux file in Honda format. ''',
-        default = 'flux/frj-solmin-mountain-aa.d')
+        default = 'flux/spl-solmax-aa.d')
     
     parser.add_argument('-o', '--outfile', dest='outfile', metavar='FILE', type=str, action='store',
                         help='file to store the output', default='flux.json')
