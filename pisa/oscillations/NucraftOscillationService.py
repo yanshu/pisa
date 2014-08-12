13f079c28b7552f40f2948f676559d133a4659ad
Lukas Schulte <schulte@physik.uni-bonn.de>
2014-08-12 19:15:33 +0200

    working on fill_osc_prob, not finished yet

--- pisa/oscillations/NucraftOscillationService.py
+++ pisa/oscillations/NucraftOscillationService.py
@@ -15,9 +15,15 @@ from pisa.oscillations.OscillationServiceBase import OscillationServiceBase
 try:
     from pisa.oscillations.nuCraft.NuCraft import NuCraft, EarthModel
 except ImportError, e:
-    logging.info("Can't load default oscillation code nuCraft: %s",e)
+    logging.error("Can't load default oscillation code nuCraft: %s",e)
     sys.exit(1)
 from pisa.resources.resources import find_resource
+
+
+def GetIceCubePID(name):
+    """Return the IceCube/Corsika Particle ID for a given particle"""
+    ptcl_dict = {'nue': 66, 'nue_bar': 67, 'numu': 68, 'numu_bar': 69}
+    return ptcl_dict[name]
 
 
 class NucraftOscillationService(OscillationServiceBase):
@@ -27,7 +33,7 @@ class NucraftOscillationService(OscillationServiceBase):
     """
     def __init__(self, ebins, czbins,
                  earth_model='oscillations/PREM_60layer.dat',
-                 detector_depth=2.0, prop_height=20.0, **kwargs):
+                 detector_depth=2.0, prop_height=None, **kwargs):
         """
         Parameters needed to instantiate a NucraftOscillationService:
         * ebins: Energy bin edges
@@ -36,12 +42,16 @@ class NucraftOscillationService(OscillationServiceBase):
                        Default: 60-layer PREM model shipped with pisa.
         * detector_depth: Detector depth in km. Default: 2.0
         * prop_height: Height in the atmosphere to begin in km. 
-                       Default: 20.0
+                       Default: None, samples from a parametrization to 
+                       the atmospheric interaction model presented in 
+                       "Path length distributions of atmospheric neutrinos",
+                       Gaisser and Stanev, PhysRevD.57.1977
         """
         OscillationServiceBase.__init__(self, ebins, czbins)
         
-        self.prop_height = prop_height
-        self.detector_depth = detector_depth
+        self.prop_height = prop_height # km above spherical Earth surface
+        self.height_mode = 3 if self.prop_height is None else 1
+        self.detector_depth = detector_depth # km below spherical Earth surface
         #TODO: find a way to convert between prob3 and NuCraft earth model files
         self.earth_model = find_resource(earth_model)
         
@@ -54,4 +64,38 @@ class NucraftOscillationService(OscillationServiceBase):
         Loops over ecen,czcen and fills the osc_prob_dict maps, with
         probabilities calculated according to NuCraft
         '''
-        #TODO: implement
+        
+        #Setup NuCraft for the given oscillation parameters
+        mass_splitting = (1., deltam21, deltam31)
+        mixing_angles = [(1,2,theta12),
+                         (1,3,theta13,deltacp),
+                         (2,3,theta23)]
+        engine = NuCraft(mass_splitting, mixing_angles,
+                         earthModel = EarthModel(self.earthModel))
+        engine.detectorDepth = self.detector_depth
+        
+        if self.prop_height is not None:
+            # Fix neutrino production height and detector depth for 
+            # simulating reactor experiments. 
+            # In this case, there should be only one zenith angle corresponding 
+            # to the baseline B. It can be calculated according to:
+            #   cos(zen) = ( (r_E - detectorDepth)**2 + B**2 (r_E + atmHeight)**2 ) \
+            #               / ( 2 * (r_E + detectorDepth) * B)
+            # with r_E = 6371. km
+            engine.atmHeight = self.prop_height
+        
+        #TODO: file through osc_prob_dict properly
+        
+        #Convert the particle into a list of IceCube particle IDs
+        ps = n.ones_like(ecen)*GetIceCubePID(prim)
+        
+        # run it
+        logging.debug("Calculating oscillation probabilites for %s at %u points..."
+                        %(prim,len(ps)))
+        probs = engine.CalcWeights((ps, ecen, np.arccos(czcen)), 
+                                   mode=self.height_mode)
+        logging.debug("...done")
+
+        #Return probabilites
+        return n.array(probs)
+
