de9277b99f53b97d99cacee45cab31ee07269490
Lukas Schulte <schulte@physik.uni-bonn.de>
2014-08-14 16:12:37 +0200

    removed old implementation (was commented anyways)

--- pisa/oscillations/TableOscillationService.py
+++ pisa/oscillations/TableOscillationService.py
@@ -82,11 +82,6 @@ class TableOscillationService(OscillationServiceBase):
         # return dictionary of smoothed, interpolated map.                     #
         ########################################################################
         
-        ### TEMPORARY SOLUTION:
-        # for now, I will grab the data from the local directory:
-        #import os
-        #maps_dir = os.getenv('PISA')+'/resources/oscProbMaps/ebins500_czbins500/'
-        # for now, no interpolation
         if deltam31 > 0.0:
           filename = os.path.join(self.datadir,'oscProbLT_dm31_0.246_th23_38.645.hdf5') 
         else:
@@ -95,31 +90,3 @@ class TableOscillationService(OscillationServiceBase):
         osc_probLT_dict = get_osc_probLT_dict_hdf5(filename)
         
         return osc_probLT_dict
-        
-        '''
-        ebinsLT = osc_probLT_dict['ebins']
-        czbinsLT = osc_probLT_dict['czbins']
-        
-        start_time = datetime.now()
-        logging.info("Getting smoothed maps...")
-
-        # do smoothing
-        smoothed_maps = {}
-        smoothed_maps['ebins'] = self.ebins
-        smoothed_maps['czbins'] = self.czbins
-        for from_nu in ['nue','numu','nue_bar','numu_bar']:
-            path_base = from_nu+'_maps'
-            to_maps = {}
-            to_nu_list = ['nue_bar','numu_bar','nutau_bar'] if 'bar' in from_nu else ['nue','numu','nutau']
-            for to_nu in to_nu_list:
-                logging.info("Getting smoothed map %s"%(from_nu+'_maps/'+to_nu))
-                to_maps[to_nu]=get_smoothed_map(osc_probLT_dict[from_nu+'_maps'][to_nu],
-                                                ebinsLT,czbinsLT,self.ebins,self.czbins)
-                
-            smoothed_maps[from_nu+'_maps'] = to_maps
-
-        total_time = (datetime.now() - start_time)
-        logging.info("Finshed getting smoothed maps. This took: %s"%(datetime.now()-start_time))
-        
-        return smoothed_maps
-        '''
