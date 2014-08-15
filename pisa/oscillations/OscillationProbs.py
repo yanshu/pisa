d853a8bc094ae6538e2977b72548aca34f484154
Lukas Schulte <schulte@physik.uni-bonn.de>
2014-08-15 14:20:29 +0200

    OscillationProbs.py can now also write HDF5 tables. With this, OscProbMaps.py is obsolete. The old, buggy lookup tables have been replaced by correct ones.

--- pisa/oscillations/OscillationProbs.py
+++ pisa/oscillations/OscillationProbs.py
@@ -13,9 +13,10 @@
 #
 
 import os,sys
-import numpy as np
 import logging
-from argparse import ArgumentParser, RawTextHelpFormatter
+from argparse import ArgumentParser
+import numpy as np
+import h5py
 from pisa.utils.utils import set_verbosity, check_binning, get_binning
 from pisa.utils.jsons import from_json, to_json, json_string
 from pisa.utils.proc import report_params, get_params, add_params
@@ -28,6 +29,33 @@ try:
     import simplejson as json
 except ImportError, e:
     import json
+
+
+def to_hdf5(oscprob_dict, filename, param_dict):
+    
+    fh = h5py.File(filename,'w')
+    
+    edata = fh.create_dataset('ebins',data=oscprob_dict['ebins'],dtype=np.float32)
+    czdata = fh.create_dataset('czbins',data=oscprob_dict['czbins'],dtype=np.float32)
+    
+    for key in oscprob_dict.keys():
+        if 'maps' in key:
+            logging.debug("  key %s",key)
+            group_base = fh.create_group(key)
+            for subkey in oscprob_dict[key].keys():
+                logging.debug("    subkey %s",subkey)
+                dset = group_base.create_dataset(subkey,data=oscprob_dict[key][subkey],dtype=np.float32)
+                dset.attrs['ebins'] = edata.ref
+                dset.attrs['czbins'] = czdata.ref
+        
+    param_group = fh.create_group("params")
+    logging.debug("  saving param dict...")
+    for key in param_dict.keys():
+        param_group.create_dataset(key,data=param_dict[key])
+        
+    fh.close()
+    return
+
 
 if __name__ == '__main__':
     
@@ -79,29 +107,55 @@ if __name__ == '__main__':
                         needed if code==table)''')
     parser.add_argument('-o', '--outfile', dest='outfile', metavar='FILE', 
                         type=str, action='store',default="osc_probs.json",
-                        help='file to store the output')
+                        help='''File to store the output. Format is guessed 
+                        from filename extension: either JSON (.json/.js) or 
+                        HDF5 (.hdf/.hd5/.hdf5). Default: osc_probs.json''')
     parser.add_argument('-v', '--verbose', action='count', default=0,
                         help='set verbosity level')
     args = parser.parse_args()
-
+    
+    #Collect settings in a dict
+    settings = vars(args).copy()
+    
     #Set verbosity level
     set_verbosity(args.verbose)
 
+    #Check requested format
+    fmt = os.path.splitext(args.outfile)[1]
+    if fmt in ['.json', '.js']:
+        fmt = 'JSON'
+    elif fmt in ['.hdf', '.hd5', '.hdf5']:
+        fmt = 'HDF5'
+    else:
+        raise NotImplementedError('Unknown file format: %s'%fmt)
+    logging.info('Will store in %s format'%fmt)
+    
     #Initialize an oscillation service
     logging.info('Using %s oscillation code'%args.code)
     if args.code=='prob3':
-      osc_service = Prob3OscillationService(**vars(args))
+      osc_service = Prob3OscillationService(**settings)
     elif args.code=='table':
-      osc_service = TableOscillationService(**vars(args))
+      osc_service = TableOscillationService(**settings)
     elif args.code=='nuCraft':
-      osc_service = NucraftOscillationService(**vars(args))
+      osc_service = NucraftOscillationService(**settings)
     else:
       raise NotImplementedError('Unknown oscillation service: %s'%args.code)
-      logging.error(exc_info=True)
     
-    logging.info("Getting osc prob maps")
+    #Do calculation
+    logging.info("Calculating oscillation probabilities")
     osc_prob_maps = osc_service.get_osc_prob_maps(**vars(args))
+    
+    #Remove irrelevant parameters from settings
+    for par in ['verbose', 'outfile', 'ebins', 'czbins']:
+        settings.pop(par)
     
     #Write out
     logging.info("Saving output to: %s",args.outfile)
-    to_json(osc_prob_maps, args.outfile)
+    if fmt=='JSON':
+        osc_prob_maps['params'] = settings
+        to_json(osc_prob_maps, args.outfile)
+    elif fmt=='HDF5':
+        to_hdf5(osc_prob_maps, args.outfile, settings)
+        logging.debug('Wrote %.2f MBytes to %s'%
+                     (os.path.getsize(args.outfile)/(1024.**2),
+                      os.path.basename(args.outfile)))
