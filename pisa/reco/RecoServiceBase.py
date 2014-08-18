2f66d6ca12c5f07b1637a5896d252aa23f20910f
Lukas Schulte <schulte@physik.uni-bonn.de>
2014-08-18 14:55:50 +0200

    Finished RecoServiceBase, needs testing

--- pisa/reco/RecoServiceBase.py
+++ pisa/reco/RecoServiceBase.py
@@ -16,6 +16,7 @@ import logging
 import numpy as np
 from itertools import product
 
+from pisa.utils.utils import is_equal_binning
 
 
 class RecoServiceBase:
@@ -41,6 +42,8 @@ class RecoServiceBase:
         
         #Get kernels already now. Can be recalculated later, if needed.
         self.kernels = self.get_reco_kernels(**kwargs)
+        self.check_kernels()
+        self.normalize_kernels()
     
     
     def get_reco_kernels(self, **kwargs):
@@ -57,6 +60,32 @@ class RecoServiceBase:
                                     %self.__class__.__name__)
     
     
+    def check_kernels(self):
+        """
+        Test whether the reco kernels have the correct shape and normalize them
+        """
+        # check axes
+        for kernel_axis, own_axis in [(self.kernels['ebins'], self.ebins),
+                                       (self.kernels['czbins'], self.czbins)]:
+            if not is_equal_binning(kernel_axis, own_axis):
+                raise ValueError("Binning of reconstruction kernel doesn't "
+                                  "match the event maps!")
+            else:
+                pass
+        # check shape of kernels
+        shape = (len(self.ebins),len(self.czbins),len(self.ebins),len(self.czbins))
+        for channel in self.kernels:
+            if not np.shape(self.kernels[channel])==shape:
+                raise IndexError('Reconstruction kernel for %s has wrong shape: '
+                                  '%s, %s' %(channel, str(shape)
+                                  str(np.shape(self.kernels[channel]))) )
+            else:
+                pass
+        # normalize
+        self.normalize_kernels()
+        return True
+    
+    
     def normalize_kernels(self):
         """
         Ensure that all reco kernels are normalized.
@@ -66,3 +95,26 @@ class RecoServiceBase:
             k_shape = np.shape(self.kernels[channel])
             for true_bin in product(range(k_shape[0]), range(k_shape[1])):
                 self.kernels[channel][true_bin] /= np.sum(self.kernels[channel][true_bin])
+    
+    
+    def apply_reconstruction(self, true_map, channel):
+        """
+        Apply the reconstruction kernel for the specified channel to the
+        provided event map.
+        """
+        # check axes
+        for map_axis, own_axis in [(true_map['ebins'], self.ebins),
+                                    (true_map['czbins'], self.czbins)]:
+            if not is_equal_binning(map_axis, own_axis):
+                raise ValueError("Binning of reconstruction kernel doesn't "
+                                  "match the event maps!")
+            else:
+                pass
+        # do smearing
+        data = true_map['map']
+        kernel = self.kernels[channel]
+        result = np.zeros_like(data)
+        for i,row in enumerate(data):
+            for j,col in enumerate(row):
+                result += data[i,j]*kernel[i,j]
+        return result
