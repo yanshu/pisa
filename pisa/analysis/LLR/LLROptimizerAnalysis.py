4a787350830fbae0ccd85435e6c55d159ba7560a
Sebastian Boeser <sboeser@physik.uni-bonn.de>
2014-10-28 10:14:30 +0100

    Number of trials now has default = 1, so is no longer required.

--- pisa/analysis/LLR/LLROptimizerAnalysis.py
+++ pisa/analysis/LLR/LLROptimizerAnalysis.py
@@ -29,7 +29,7 @@ parser.add_argument('-t','--template_settings',type=str,
 parser.add_argument('-m','--minimizer_settings',type=str,
                     metavar='JSONFILE', required = True,
                     help='''Settings related to the optimizer used in the LLR analysis.''')
-parser.add_argument('-n','--ntrials',type=int, required = True, default = 1,
+parser.add_argument('-n','--ntrials',type=int, default = 1,
                     help="Number of trials to run")
 parser.add_argument('-s','--save_steps',action='store_true',default=False,
                     help="Save all steps the optimizer takes.")
