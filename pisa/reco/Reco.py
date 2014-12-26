Corrected normalization error for parameterized method. Now param method is ready to run. (--> KDE method still broken tho <--)

pisa/reco/Reco.py
@@ -77,12 +77,17 @@ def get_reco_maps(true_event_maps, reco_service=None,e_reco_scale=None,
                     for icz,cz in enumerate(czbins[:-1]):
                         # Get kernel at these true parameters from 4D hist
                         kernel = kernels[ie,icz]
+                        #if flavor == 'numu' and int_type == 'cc': #ie == 5 and icz == 5:
+                        #    print "  egy: %.2f, cz: %.2f "%(egy,cz)
+                        #    print "  kernel: ",kernel[ie]
+                        #    print "  sum: ",np.sum(kernel)
+                        #    raw_input("PAUSED")
                         reco_evt_rate += true_evt_rate[ie,icz]*kernel
 
             reco_maps[flavor+'_'+int_type] = {'map':reco_evt_rate,
                                               'ebins':ebins,
                                               'czbins':czbins}
-            physics.trace("Total counts for %s %s: %.2f"%(flavor, int_type, 
+            physics.trace("AFTER RECO: Total counts for %s %s: %.2f"%(flavor, int_type,
                                                           np.sum(reco_evt_rate)))
 
     #Finally sum up all the NC contributions

pisa/reco/RecoServiceBase.py
@@ -24,16 +24,28 @@ from pisa.utils.proc import report_params, get_params, add_params
 def normalize_kernels(kernels):
     """
     Ensure that all reco kernels are normalized.
+
+    NOTE: TCA (12/26/2014) -> In PaPA, kernels are normalized one way,
+    and they are normalized a different way here. I am changing them
+    to the PaPA way, because I think that is correct. I.e. in PaPA, we
+    assume \int \int _{all reco space} kernel(E_t,cz_t) dE dCZ = 1,
+    whereas the "old" PISA way was to ensure the sum of the kernel
+    values was equal to 1. The PISA way makes sure that the number of
+    events before and after reconstructions are applied is constant,
+    but this is not going to be the case with imperfect resolutions.
+
+    UPDATE: NOW PUTTING THIS INTO _get_reco_kernels() itself
     """
     logging.debug('Normalizing reconstruction kernels')
-    for flavour in kernels:
-        if flavour in ['ebins', 'czbins']: continue
-        for interaction in kernels[flavour]:
-            k_shape = np.shape(kernels[flavour][interaction])
-            for true_bin in product(range(k_shape[0]), range(k_shape[1])):
-                kernel_sum = np.sum(kernels[flavour][interaction][true_bin])
-                if kernel_sum > 0.:
-                    kernels[flavour][interaction][true_bin] /= kernel_sum
+
+    #for flavour in kernels:
+    #    if flavour in ['ebins', 'czbins']: continue
+    #    for interaction in kernels[flavour]:
+    #        k_shape = np.shape(kernels[flavour][interaction])
+    #        for true_bin in product(range(k_shape[0]), range(k_shape[1])):
+    #            kernel_sum = np.sum(kernels[flavour][interaction][true_bin])
+    #            if kernel_sum > 0.:
+    #                kernels[flavour][interaction][true_bin] /= kernel_sum
 
     return kernels
 
@@ -70,8 +82,7 @@ class RecoServiceBase:
         """
         kernels = self._get_reco_kernels(**kwargs)
 
-        if self.check_kernels(kernels):
-            return normalize_kernels(kernels)
+        if self.check_kernels(kernels): return kernels
 
 
     def _get_reco_kernels(self, **kwargs):