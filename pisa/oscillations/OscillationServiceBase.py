Fixed several issues regarding the reco stage-normalization issues, as well as modifying the RecoServiceKDE class to work with the KDE notebook. Secondly, increased smoothed maps genration time by an order of magnitude, by using the scipy.stats.binned_statistic_2d function.

pisa/oscillations/OscillationServiceBase.py
@@ -9,8 +9,7 @@
 import sys
 import numpy as np
 from pisa.utils.log import logging, profile
-from pisa.utils.utils import get_smoothed_map
-from pisa.utils.utils import get_bin_centers, is_coarser_binning, is_linear, is_logarithmic
+from pisa.utils.utils import get_smoothed_map, get_bin_centers, is_coarser_binning, is_linear, is_logarithmic, Timer
 
 
 def check_fine_binning(fine_bins, coarse_bins):
@@ -97,7 +96,9 @@ class OscillationServiceBase:
         """
         #Get the finely binned maps as implemented in the derived class
         logging.info('Retrieving finely binned maps')
-        fine_maps = self.get_osc_probLT_dict(**kwargs)
+        with Timer(verbose=False) as t:
+            fine_maps = self.get_osc_probLT_dict(**kwargs)
+        #print "  => elapsed time to get all fine maps: %s sec"%t.secs
 
         logging.info("Smoothing fine maps...")
         profile.info("start smoothing maps")
@@ -105,18 +106,22 @@ class OscillationServiceBase:
         smoothed_maps['ebins'] = self.ebins
         smoothed_maps['czbins'] = self.czbins
 
-        for from_nu, tomap_dict in fine_maps.items():
-            if 'bins' in from_nu: continue
-            new_tomaps = {}
-            for to_nu, tomap in tomap_dict.items():
-                logging.debug("Getting smoothed map %s/%s"%(from_nu,to_nu))
-                new_tomaps[to_nu] = get_smoothed_map(tomap,
-                                         fine_maps['ebins'],
-                                         fine_maps['czbins'],
-                                         self.ebins, self.czbins)
-            smoothed_maps[from_nu] = new_tomaps
-
+        with Timer(verbose=False) as t:
+            for from_nu, tomap_dict in fine_maps.items():
+                if 'vals' in from_nu: continue
+                new_tomaps = {}
+                for to_nu, pvals in tomap_dict.items():
+                    logging.debug("Getting smoothed map %s/%s"%(from_nu,to_nu))
+                    new_tomaps[to_nu] = get_smoothed_map(pvals,
+                                                         fine_maps['evals'],
+                                                         fine_maps['czvals'],
+                                                         self.ebins, self.czbins)
+                smoothed_maps[from_nu] = new_tomaps
+        #print"  ==> elapsed time to smooth maps: %s sec"%t.secs
         profile.info("stop smoothing maps")
+
+        from pisa.utils.jsons import to_json
+        to_json(smoothed_maps,'sm_maps_test.json')
 
         return smoothed_maps
 
@@ -148,15 +153,16 @@ class OscillationServiceBase:
         ecen = get_bin_centers(ebins)
         czcen = get_bin_centers(czbins)
 
-        osc_prob_dict = {'ebins':ebins, 'czbins':czbins}
-        shape = (len(ecen),len(czcen))
+        osc_prob_dict = {}
         for nu in ['nue_maps','numu_maps','nue_bar_maps','numu_bar_maps']:
             isbar = '_bar' if 'bar' in nu else ''
-            osc_prob_dict[nu] = {'nue'+isbar: np.zeros(shape,dtype=np.float32),
-                                 'numu'+isbar: np.zeros(shape,dtype=np.float32),
-                                 'nutau'+isbar: np.zeros(shape,dtype=np.float32)}
+            osc_prob_dict[nu] = {'nue'+isbar: [],
+                                 'numu'+isbar: [],
+                                 'nutau'+isbar: [],}
 
-        self.fill_osc_prob(osc_prob_dict, ecen, czcen, **kwargs)
+        evals,czvals = self.fill_osc_prob(osc_prob_dict, ecen, czcen, **kwargs)
+        osc_prob_dict['evals'] = evals
+        osc_prob_dict['czvals'] = czvals
 
         return osc_prob_dict
 

pisa/oscillations/Prob3OscillationService.py
@@ -19,6 +19,7 @@ from pisa.resources.resources import find_resource
 from pisa.utils.jsons import to_json
 from pisa.utils.proc import get_params, report_params
 
+
 class Prob3OscillationService(OscillationServiceBase):
     """
     This class handles all tasks related to the oscillation
@@ -67,17 +68,20 @@ class Prob3OscillationService(OscillationServiceBase):
         sin2th13Sq = np.sin(2.0*theta13)**2
         sin2th23Sq = np.sin(2.0*theta23)**2
 
+        evals = []
+        czvals = []
         total_bins = int(len(ecen)*len(czcen))
         mod = total_bins/50
-        ibin = 0
         loglevel = logging.root.getEffectiveLevel()
-        for icz, coszen in enumerate(czcen):
+        for ie,energy in enumerate(ecen):
+            for icz, coszen in enumerate(czcen):
+                index = int((ie+1)*(icz+1) - 1)
+                evals.append(energy)
+                czvals.append(coszen)
 
-            for ie,energy in enumerate(ecen):
                 if energy_scale is not None: energy*=energy_scale
-                ibin+=1
                 if loglevel <= logging.INFO:
-                    if (ibin%mod) == 0:
+                    if ( ((len(ecen)+1)*(len(czcen)+1))%mod ) == 0:
                         sys.stdout.write(".")
                         sys.stdout.flush()
 
@@ -99,7 +103,8 @@ class Prob3OscillationService(OscillationServiceBase):
                     nu = nu+'_maps'
                     for to_nu in neutrinos:
                         nu_f = nu_barger[to_nu]
-                        osc_prob_dict[nu][to_nu][ie][icz]=self.barger_prop.GetProb(nu_i,nu_f)
+                        osc_prob_dict[nu][to_nu].append(self.barger_prop.GetProb(nu_i,
+                                                                                 nu_f))
 
                 ########### SECOND FOR ANTINEUTRINOS ##########
                 kNuBar = -1
@@ -113,11 +118,12 @@ class Prob3OscillationService(OscillationServiceBase):
                     nu+='_maps'
                     for to_nu in anti_neutrinos:
                         nu_f = nu_barger[to_nu]
-                        osc_prob_dict[nu][to_nu][ie][icz] = self.barger_prop.GetProb(nu_i,nu_f)
+                        osc_prob_dict[nu][to_nu].append(self.barger_prop.GetProb(nu_i,
+                                                                                 nu_f))
 
         if loglevel <= logging.INFO:
             sys.stdout.write("\n")
 
         profile.info("stop oscillation calculation")
 
-        return
+        return evals,czvals

pisa/reco/RecoServiceKDE.py
@@ -25,6 +25,19 @@ from pisa.utils.log import logging, physics
 class RecoServiceKDE(RecoServiceBase):
     """
     Creates reconstruction kernels using Kernel Density Estimation (KDE).
+
+    NOTE: (tca) 22 Jan 2015 - For some reason V36 (noisy) is not
+    being well characterized by the KDE automatically, like V15
+    was. It would be nice to get to the bottom of why this is,
+    but I haven't been able to do so yet. For now, it's recommended
+    to either:
+
+    1) Use the notebook in $PISA/pisa/i3utils to find the KDEs
+    and convert them to kernelfiles
+
+    or
+
+    2) Use the parameterization method rather than KDEs.
     """
 
     def __init__(self, ebins, czbins, reco_kde_file=None, **kwargs):
@@ -45,10 +58,11 @@ class RecoServiceKDE(RecoServiceBase):
     def _get_reco_kernels(self, reco_kde_file=None,e_reco_scale=None,
                           cz_reco_scale=None,**kwargs):
 
+        if reco_kde_file is None:
+            logging.warn("Did not receive a reco_kde_file-> returning WITHOUT constructing kernels.")
+            return
 
         if self.kernels is not None:
-            # Scale reconstruction widths
-            #self.apply_reco_scales(e_reco_scale, cz_reco_scale)
             return self.kernels
 
         # This is the step that is analagous to finding
@@ -70,8 +84,6 @@ class RecoServiceKDE(RecoServiceBase):
         reco energy, reco coszen) for each flavour and interaction
         type.
         '''
-
-#TODO: implement reco scales here
 
         # get binning information
         evals, esizes = get_bin_centers(self.ebins), get_bin_sizes(self.ebins)
@@ -148,7 +160,7 @@ class RecoServiceKDE(RecoServiceBase):
         """
         Constructs the 1D energy and coszen KDEs from the data in
         kdefile, and stores them in self.kde_dict. These resulting
-        1D KDEs are then used to create the full 4D parameterized
+        1D KDEs can be used to create the full 4D parameterized
         kernels in calculate_kernels()
         """
 
@@ -213,7 +225,7 @@ class RecoServiceKDE(RecoServiceBase):
         ecen = get_bin_centers(self.ebins)
         kde_dict = {'energy': [],
                      'coszen': []}
-        min_events = 500#800
+        min_events = 500
         min_bin_width = 1.0
         for ie,energy in enumerate(ecen):
             min_edge = self.ebins[ie]
@@ -223,7 +235,7 @@ class RecoServiceKDE(RecoServiceBase):
                                           e_true < max_edge]),axis=0)
             nevents = np.sum(in_bin)
             # TESTING:
-            #logging.trace("working on energy %.2f, nevents: %.2f "%(energy,nevents))
+            logging.trace("working on energy %.2f, nevents: %.2f "%(energy,nevents))
             if ((nevents < min_events) or (bin_width < min_bin_width) ):
                 logging.trace("  Increasing bin size for-> energy: %.2f, nevents: %.2d"
                               %(energy,nevents))
@@ -254,14 +266,6 @@ class RecoServiceKDE(RecoServiceBase):
             e_res_data = e_reco[in_bin] - e_true[in_bin]
             cz_res_data = cz_reco[in_bin] - cz_true[in_bin]
             egy_kde = gaussian_kde(e_res_data)
-
-            ########## TESTING #############
-            bw = max(0.02,2*energy/500.0)
-            egy_kde.set_bandwidth(bw)
-            cz_kde = gaussian_kde(cz_res_data)
-            bw = 0.1
-            cz_kde.set_bandwidth(bw)
-            #################################
 
             kde_dict['energy'].append(egy_kde)
             kde_dict['coszen'].append(cz_kde)