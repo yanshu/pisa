f6ed7468fdf03f59dee5d89411e46ffe75a529ba
Thomas Ehrhardt <tehrhard@uni-mainz.de>
2015-03-17 13:03:21 +0100

    Minor code optimisation.

--- pisa/oscillations/NucraftOscillationService.py
+++ pisa/oscillations/NucraftOscillationService.py
@@ -49,7 +49,7 @@ class NucraftOscillationService(OscillationServiceBase):
         self.prop_height = prop_height # km above spherical Earth surface
  	''' height_mode = 0 ensures that interaction takes place at chosen height '''
 	''' whereas height_mode = 1 samples single altitude from distribution '''
-        self.height_mode = 3 if self.prop_height is 'sample' else 0 # 1
+        self.height_mode = 3 if self.prop_height is 'sample' else 0
         self.detector_depth = detector_depth # km below spherical Earth surface
         self.num_prec = osc_precision
         self.get_earth_model(earth_model)
@@ -86,7 +86,7 @@ class NucraftOscillationService(OscillationServiceBase):
 
         # Make input arrays in correct format (nucraft input type 1)
 	zs, es = np.meshgrid(czcen, ecen)
-        es, zs = es.flatten(), zs.flatten()
+        zs, es = zs.flatten(), es.flatten()
 	# we need flat lists with probabilities for further processing
 	shape = int(len(ecen)*len(czcen))
 
@@ -117,15 +117,8 @@ class NucraftOscillationService(OscillationServiceBase):
                 sec_key = sec+'_bar' if 'bar' in prim else sec
                 osc_prob_dict[prim][sec_key] = probs[i]
 
-	# fix: return energy-cz values as requested by OscillationServiceBase
-	evals = []
-	czvals = []
-	for ie,energy in enumerate(ecen):
-            for icz, coszen in enumerate(czcen):
-	        evals.append(energy)
-		czvals.append(coszen)
-
-        return evals,czvals
+        evals,czvals = np.meshgrid(ecen,czcen,indexing='ij')
+        return evals.flatten(),czvals.flatten()
 
 
     def get_earth_model(self, model):
