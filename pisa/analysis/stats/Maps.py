f721eb9546807385c3166afafef96c25bee5bbbe
Thomas Ehrhardt <tehrhard@uni-mainz.de>
2015-04-01 00:58:30 +0200

    Modified implementation of nue_numu_ratio and nu_nubar_ratio systematics, such that total fluxes/event counts are conserved.

--- pisa/analysis/stats/Maps.py
+++ pisa/analysis/stats/Maps.py
@@ -11,6 +11,42 @@ import numpy as np
 from pisa.analysis.stats.LLHStatistics import get_random_map
 from pisa.utils.log import logging
 
+
+def apply_ratio_scale(orig_maps, key1, key2, ratio_scale, is_flux_scale, int_type = None):
+    '''
+    Scales the ratio of the entries of two maps, conserving the total.
+    '''
+
+    if is_flux_scale:
+        log_str = 'flux'
+    else:
+        log_str = 'event rate (%s)'%int_type
+
+    if not is_flux_scale:
+	# we have maps of event counts of a certain interaction type
+        orig_sum = orig_maps[key1][int_type]['map'] + orig_maps[key2][int_type]['map']
+        orig_total1 = orig_maps[key1][int_type]['map'].sum()
+        orig_total2 = orig_maps[key2][int_type]['map'].sum()
+        orig_ratio = orig_maps[key1][int_type]['map'] / orig_maps[key2][int_type]['map']
+    else:
+        # we have flux_maps
+        orig_sum = orig_maps[key1]['map'] + orig_maps[key2]['map']
+        orig_total1 = orig_maps[key1]['map'].sum()
+        orig_total2 = orig_maps[key2]['map'].sum()
+        orig_ratio = orig_maps[key1]['map'] / orig_maps[key2]['map']
+
+    # conserved total:
+    scaled_map2 = orig_sum / (1 + ratio_scale*orig_ratio)
+    scaled_map1 = ratio_scale*orig_ratio*scaled_map2
+
+    logging.trace(' %s / %s %s ratio before scaling: %.3f'%(key1, key2, log_str,
+                    orig_total1/orig_total2))
+    logging.trace(' %s / %s %s ratio after scaling with %.2f: %.3f'%(key1, key2, log_str,
+                    ratio_scale, scaled_map1.sum()/scaled_map2.sum()))
+
+    return scaled_map1, scaled_map2
+
+
 def get_pseudo_data_fmap(template_maker,fiducial_params,seed=None,chan=None):
     '''
     Creates a true template from fiducial_params, then uses Poisson statistics
