474548324e8e81673cc73ce995e1d8d1b6623a14
yanshu <huangfeifei91@gmail.com>
2015-04-16 18:14:16 -0400

    modified LLHStatistics.py

--- pisa/analysis/stats/LLHStatistics.py
+++ pisa/analysis/stats/LLHStatistics.py
@@ -10,24 +10,25 @@
 from pisa.utils.jsons import from_json
 import numpy as np
 from scipy.stats import poisson,skellam
+from scipy.special import iv
 
 def get_binwise_llh(pseudo_data,template,template_params):
     '''
     computes the log-likelihood (llh) of the pseudo_data from the
     template, where each input is expected to be a 2d numpy array
     '''
+    if not np.alltrue(template >= 0.0):
+        raise ValueError("Template must have all bins >= 0.0! Template generation bug?")
     if template_params['residual_up_down']:
-        if len(template)!=2:
-            raise ValueError("Under current template settings, template must be an array of two arrays(i.e. up-going and down-going array)!")
-        if not np.alltrue(template >= 0.0):
-            raise ValueError("Template must have all bins >= 0.0! Template generation bug?")
-        #totalLLH = np.sum(skellam.logpmf(pseudo_data,np.float64(template[0]),np.float64(template[1])))     #causes overflow when mu1,mu2 gets too big
         mu1 = np.float64(template[0])
         mu2 = np.float64(template[1])
-        totalLLH = np.sum(-mu1-mu2+0.5*pseudo_data*np.log(mu1/mu2)+np.log(iv(pseudo_data,2*np.sqrt(mu1*mu2))))
+        if len(template)!=2:
+            raise ValueError("Under current template settings, template must be an array of two arrays(i.e. up-going and down-going array)!")
+        #totalLLH = np.sum(skellam.logpmf(pseudo_data,np.float64(template[0]),np.float64(template[1])))    
+                                              # causes overflow when mu1,mu2 gets too big, it calculates pmf first, then take the log.
+        totalLLH = np.sum(-mu1-mu2+0.5*pseudo_data*np.log(mu1/mu2)+np.log(iv(pseudo_data,2*np.sqrt(mu1*mu2)))) 
+                                              # better, but could also end up inf if (mu1*mu2) is too large, e.g. iv(0,2*np.sqrt(127444)) ~ 1.7961e+308
     else:
-        if not np.alltrue(template >= 0.0):
-            raise ValueError("Template must have all bins >= 0.0! Template generation bug?")
         totalLLH = np.sum(np.log(poisson.pmf(pseudo_data,np.float64(template))))
 
     return totalLLH
