5f1167be3b62432aa3650f9dab07981697d9f74c
yanshu <huangfeifei91@gmail.com>
2015-04-16 15:18:08 -0400

    changed logpmf() calculation

--- pisa/analysis/stats/LLHStatistics.py
+++ pisa/analysis/stats/LLHStatistics.py
@@ -21,7 +21,10 @@ def get_binwise_llh(pseudo_data,template,template_params):
             raise ValueError("Under current template settings, template must be an array of two arrays(i.e. up-going and down-going array)!")
         if not np.alltrue(template >= 0.0):
             raise ValueError("Template must have all bins >= 0.0! Template generation bug?")
-        totalLLH = np.sum(skellam.logpmf(pseudo_data,np.float64(template[0]),np.float64(template[1])))
+        #totalLLH = np.sum(skellam.logpmf(pseudo_data,np.float64(template[0]),np.float64(template[1])))     #causes overflow when mu1,mu2 gets too big
+        mu1 = np.float64(template[0])
+        mu2 = np.float64(template[1])
+        totalLLH = np.sum(-mu1-mu2+0.5*pseudo_data*np.log(mu1/mu2)+np.log(iv(pseudo_data,2*np.sqrt(mu1*mu2))))
     else:
         if not np.alltrue(template >= 0.0):
             raise ValueError("Template must have all bins >= 0.0! Template generation bug?")
