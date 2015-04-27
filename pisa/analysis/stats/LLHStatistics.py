a0a664d5ab77f7bfb89f09151defa15ae93269c7
Timothy Arlen (laptop) <timothyarlen@gmail.com>
2015-04-26 21:22:33 -0400

    Fixed problem of converting Asimov data set to integer, and now asimov expectation is exactly the template expectation values

--- pisa/analysis/stats/LLHStatistics.py
+++ pisa/analysis/stats/LLHStatistics.py
@@ -7,29 +7,46 @@
 # files in this module.
 #
 
-from pisa.utils.jsons import from_json
+import re
 import numpy as np
+from scipfy.special import gamma
 from scipy.stats import poisson
 
+from pisa.utils.jsons import from_json
+
+def poisson_pmf_float(data,expectation):
+    """
+    When the data set is not integer based, we need a different way to
+    calculate the poisson likelihood, so we'll use this version, which
+    is appropriate for float data types.
+    """
+    return (expectation**data)*np.exp(-expectation)/gamma(data+1)
+
 def get_binwise_llh(pseudo_data,template):
-    '''
-    computes the log-likelihood (llh) of the pseudo_data from the
+    """
+    Computes the log-likelihood (llh) of the pseudo_data from the
     template, where each input is expected to be a 2d numpy array
-    '''
+    """
 
     if not np.alltrue(template >= 0.0):
         raise ValueError("Template must have all bins >= 0.0! Template generation bug?")
 
-    totalLLH = np.sum(np.log(poisson.pmf(pseudo_data,np.float64(template))))
+    if bool(re.match('^int',pseudo_data.dtype.name)):
+        totalLLH = np.sum(np.log(poisson.pmf(pseudo_data,template)))
+    elif bool(re.match('^float',pseudo_data.dtype.name)):
+        totalLLH = np.sum(np.log(poisson_pmf_float(pseudo_data,template)))
+    else:
+        raise ValueError(
+            "Unknown pseudo_data dtype: %s. Must be float or int!"%psuedo_data.dtype)
 
     return totalLLH
 
 def get_random_map(template, seed=None):
-    '''
+    """
     Gets an event map with integer entries from non-integer entries
     (in general) in the template, varied according to Poisson
     statistics.
-    '''
+    """
     #Set the seed if given
     if not seed is None:
         np.random.seed(seed=seed)
