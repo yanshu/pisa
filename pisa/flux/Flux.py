9f512102579b052b368f06a94a2ffb3da9f82fa3
Timothy Arlen (laptop) <timothyarlen@gmail.com>
2015-05-02 02:36:10 -0400

    Added numu spectral index as nuisance parameter

--- pisa/flux/Flux.py
+++ pisa/flux/Flux.py
@@ -17,18 +17,20 @@ import os
 import sys
 import numpy as np
 from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter
+
+from pisa.analysis.stats.Maps import apply_ratio_scale
+from pisa.flux.HondaFluxService import HondaFluxService, primaries
 from pisa.utils.log import logging, physics, set_verbosity
 from pisa.utils.jsons import from_json, to_json, json_string
 from pisa.utils.proc import report_params, get_params, add_params
-from pisa.analysis.stats.Maps import apply_ratio_scale
-from pisa.flux.HondaFluxService import HondaFluxService, primaries
+from pisa.utils.utils import get_bin_centers
 
 def apply_nue_numu_ratio(flux_maps, nue_numu_ratio):
-    '''
+    """
     Applies the nue_numu_ratio systematic to the flux maps
     and returns the scaled maps. The actual calculation is
     done by apply_ratio_scale.
-    '''
+    """
     # keep both nu and nubar flux constant
     scaled_nue_flux, scaled_numu_flux = apply_ratio_scale(
         orig_maps = flux_maps,
@@ -51,9 +53,47 @@ def apply_nue_numu_ratio(flux_maps, nue_numu_ratio):
 
     return flux_maps
 
+def apply_delta_index(flux_maps, delta_index, egy_med):
+    """
+    Applies the spectral index systematic to the flux maps by scaling
+    each bin with (egy_cen/egy_med)^(-delta_index), preserving the total
+    integral flux  Note that only the numu/numu_bar are scaled, because
+    the nue_numu_ratio will handle the systematic on the nue flux.
+    """
 
-def get_flux_maps(flux_service, ebins, czbins, nue_numu_ratio, energy_scale, **kwargs):
-    '''
+    for flav in ['numu','numu_bar']:
+        ecen = get_bin_centers(flux_maps[flav]['ebins'])
+        scale = np.power((ecen/egy_med),delta_index)
+        flux_map = flux_maps[flav]['map']
+        total_flux = flux_map.sum()
+        logging.trace("flav: %s, total counts before scale: %f"%(flav,total_flux))
+        scaled_flux = (flux_map.T*scale).T
+        scaled_flux *= (total_flux/scaled_flux.sum())
+        flux_maps[flav]['map'] = scaled_flux
+        logging.trace("flav: %s, total counts after scale: %f"%
+                      (flav,flux_maps[flav]['map'].sum()))
+
+    return flux_maps
+
+def get_median_energy(flux_map):
+    """Returns the median energy of the flux_map-expected to be a dict
+    with keys 'map', 'ebins', 'czbins'
+    """
+
+    # This finds the mean (Not used)
+    #flux1D = np.sum(flux_map['map'],axis=1)
+    #ecen = get_bin_centers(flux_map['ebins'])
+    #energy = np.sum(flux1D*ecen)/flux1D.sum()
+    #logging.trace("Median energy: %f",energy)
+
+    ecen = get_bin_centers(flux_map['ebins'])
+    energy = ecen[len(ecen)/2]
+
+    return energy
+
+def get_flux_maps(flux_service, ebins, czbins, nue_numu_ratio, energy_scale,
+                  atm_delta_index,**kwargs):
+    """
     Get a set of flux maps for the different primaries.
 
     \params:
@@ -65,34 +105,37 @@ def get_flux_maps(flux_service, ebins, czbins, nue_numu_ratio, energy_scale, **k
         constant. The adjusted ratios are given by
         "nue_numu_ratio * original ratio".
       * energy_scale - factor to scale energy bin centers by
-    '''
+      * atm_delta_index  - change in spectral index from fiducial
+    """
 
-    #Be verbose on input
+    # Be verbose on input
     params = get_params()
     report_params(params, units = [''])
 
-    #Initialize return dict
+    # Initialize return dict
     maps = {'params': params}
 
     for prim in primaries:
 
-        #Get the flux for this primary
+        # Get the flux for this primary
         maps[prim] = {'ebins': ebins,
                       'czbins': czbins,
                       'map': flux_service.get_flux(ebins*energy_scale,czbins,prim)}
 
-        #be a bit verbose
+        # be a bit verbose
         logging.trace("Total flux of %s is %u [s^-1 m^-2]"%
                       (prim,maps[prim]['map'].sum()))
 
     # now scale the nue(bar) / numu(bar) flux ratios, keeping the total
     # flux (nue + numu, nue_bar + numu_bar) constant, or return unscaled maps:
-    return apply_nue_numu_ratio(maps, nue_numu_ratio) if nue_numu_ratio != 1.0 else maps
+    scaled_maps = apply_nue_numu_ratio(maps, nue_numu_ratio) if nue_numu_ratio != 1.0 else maps
 
-    #if nue_numu_ratio != 1.:
-    #    return apply_nue_numu_ratio(maps, nue_numu_ratio)
-    # else: no scaling to be applied
-    #return maps
+    median_energy = get_median_energy(maps['numu'])
+
+    if atm_delta_index != 0.0:
+        scaled_maps = apply_delta_index(scaled_maps, atm_delta_index, median_energy)
+
+    return scaled_maps
 
 
 if __name__ == '__main__':
@@ -112,6 +155,8 @@ if __name__ == '__main__':
                         default = 'flux/spl-solmax-aa.d')
     parser.add_argument('--nue_numu_ratio',metavar='FLOAT',type=float,
                         help='''Factor to scale nue_flux by''',default=1.0)
+    parser.add_argument('--delta_index',metavar='FLOAT',type=float,
+                        default=0.0,help='''Shift in spectral index of numu''')
     parser.add_argument('--energy_scale',metavar='FLOAT',type=float,
                         help='''Factor to scale TRUE energy by''',default=1.0)
     parser.add_argument('-o', '--outfile', dest='outfile', metavar='FILE',
@@ -133,8 +178,9 @@ if __name__ == '__main__':
     flux_model = HondaFluxService(args.flux_file)
 
     #get the flux
-    flux_maps = get_flux_maps(flux_model,args.ebins,args.czbins,
-                              args.nue_numu_ratio,args.energy_scale)
+    flux_maps = get_flux_maps(
+        flux_model,args.ebins,args.czbins,args.nue_numu_ratio,args.energy_scale,
+        args.delta_index)
 
 
     #write out to a file
