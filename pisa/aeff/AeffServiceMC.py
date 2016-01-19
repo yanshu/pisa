fe28afae35d47642c685556735aeb75acf0f0675
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-01-18 22:18:34 -0500

    Updated naming of "kind" -> flavInt

--- pisa/aeff/AeffServiceMC.py
+++ pisa/aeff/AeffServiceMC.py
@@ -11,15 +11,15 @@ import numpy as np
 from pisa.utils.log import logging, set_verbosity
 from pisa.utils.utils import get_bin_centers, get_bin_sizes
 import pisa.utils.flavInt as flavInt
-import pisa.utils.events as EVENTS
+import pisa.utils.events as events
 
 
 class AeffServiceMC:
-    '''
+    """
     Takes the weighted effective area files, and creates a dictionary
     of the 2D effective area in terms of energy and coszen, for each
     flavor (nue,nue_bar,numu,...) and interaction type (CC, NC)
-    '''
+    """
 
     def __init__(self, ebins, czbins, aeff_weight_file, **kwargs):
         self.ebins = ebins
@@ -27,17 +27,17 @@ class AeffServiceMC:
         logging.info('Initializing AeffServiceMC...')
 
         logging.info('Extracting events from file: %s' % (aeff_weight_file))
-        events = EVENTS.Events(aeff_weight_file)
+        evts = events.Events(aeff_weight_file)
 
-        self.aeff_dict = flavInt.FIData()
+        self.aeff_dict = flavInt.FlavIntData()
         logging.info("Creating effective area dict...")
-        for kind in flavInt.ALL_KINDS:
-            logging.debug("Working on %s effective areas" % kind)
+        for flavint in flavInt.ALL_NUFLAVINTS:
+            logging.debug("Working on %s effective areas" % flavint)
             bins = (self.ebins, self.czbins)
             aeff_hist,_,_ = np.histogram2d(
-                events.get(kind, 'true_energy'),
-                events.get(kind, 'true_coszen'),
-                weights=events.get(kind, 'weighted_aeff'),
+                evts.get(flavint, 'true_energy'),
+                evts.get(flavint, 'true_coszen'),
+                weights=evts.get(flavint, 'weighted_aeff'),
                 bins=bins
             )
             # Divide histogram by bin ExCZ "widths" to convert to aeff
@@ -45,11 +45,9 @@ class AeffServiceMC:
             czbin_sizes = 2.0*np.pi*get_bin_sizes(czbins)
             bin_sizes = np.meshgrid(czbin_sizes, ebin_sizes)
             aeff_hist /= np.abs(bin_sizes[0]*bin_sizes[1])
-            # Save the result to the FIData object
-            self.aeff_dict.set(kind, aeff_hist)
+            # Save the result to the FlavIntData object
+            self.aeff_dict.set(flavint, aeff_hist)
 
     def get_aeff(self, **kwargs):
-        '''
-        Returns the effective area dictionary
-        '''
+        """Returns the effective area dictionary"""
         return self.aeff_dict
