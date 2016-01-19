6b8a8562270334023406e4090f49b2d0a2e5431e
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-01-18 22:25:34 -0500

    Aesthetic touchups

--- pisa/aeff/AeffServiceMC.py
+++ pisa/aeff/AeffServiceMC.py
@@ -1,15 +1,15 @@
 #
-# Creates the effective areas from a simfile, then returns them
-# re-scaled as desired.
-#
 # author: Timothy C. Arlen
 #
 # date:   April 8, 2014
-#
+"""
+Creates the effective areas from a simfile, then returns them
+re-scaled as desired.
+"""
 
 import numpy as np
-from pisa.utils.log import logging, set_verbosity
-from pisa.utils.utils import get_bin_centers, get_bin_sizes
+from pisa.utils.log import logging
+from pisa.utils.utils import get_bin_sizes
 import pisa.utils.flavInt as flavInt
 import pisa.utils.events as events
 
@@ -29,12 +29,12 @@ class AeffServiceMC:
         logging.info('Extracting events from file: %s' % (aeff_weight_file))
         evts = events.Events(aeff_weight_file)
 
-        self.aeff_dict = flavInt.FlavIntData()
+        self.aeff = flavInt.FlavIntData()
         logging.info("Creating effective area dict...")
         for flavint in flavInt.ALL_NUFLAVINTS:
             logging.debug("Working on %s effective areas" % flavint)
             bins = (self.ebins, self.czbins)
-            aeff_hist,_,_ = np.histogram2d(
+            aeff_hist, _, _ = np.histogram2d(
                 evts.get(flavint, 'true_energy'),
                 evts.get(flavint, 'true_coszen'),
                 weights=evts.get(flavint, 'weighted_aeff'),
@@ -46,8 +46,8 @@ class AeffServiceMC:
             bin_sizes = np.meshgrid(czbin_sizes, ebin_sizes)
             aeff_hist /= np.abs(bin_sizes[0]*bin_sizes[1])
             # Save the result to the FlavIntData object
-            self.aeff_dict.set(flavint, aeff_hist)
+            self.aeff.set(flavint, aeff_hist)
 
     def get_aeff(self, **kwargs):
         """Returns the effective area dictionary"""
-        return self.aeff_dict
+        return self.aeff
