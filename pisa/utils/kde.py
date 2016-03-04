743e30f2fdf431792147689229cafeb9f15d3f16
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-03-04 11:51:29 -0500

    superficial formatting changes

--- pisa/utils/kde.py
+++ pisa/utils/kde.py
@@ -1,14 +1,8 @@
 #!/usr/bin/env python, gaussians
-
-#
-# kde.py
-#
-# An implementation of the kde bandwidth selection method outlined in:
-#
-# Z. I. Botev, J. F. Grotowski, and D. P. Kroese. Kernel density
-# estimation via diffusion. The Annals of Statistics, 38(5):2916-2957, 2010.
 #
 # Based on the implementation in Matlab by Zdravko Botev.
+# Z. I. Botev, J. F. Grotowski, and D. P. Kroese. Kernel density
+# estimation via diffusion. The Annals of Statistics, 38(5):2916-2957, 2010.
 #
 # Daniel B. Smith, PhD
 # Updated 1-23-2013
@@ -19,41 +13,41 @@
 #     fixed_point
 # functions since these were derived from Botev's original work (this license
 # applies to any future code derived from those functions as well):
-# ==============================================================================
-#     Copyright (c) 2007, Zdravko Botev
-#     All rights reserved.
+# ============================================================================
+#   Copyright (c) 2007, Zdravko Botev
+#   All rights reserved.
 #
-#     Redistribution and use in source and binary forms, with or without
-#     modification, are permitted provided that the following conditions are
-#     met:
+#   Redistribution and use in source and binary forms, with or without
+#   modification, are permitted provided that the following conditions are
+#   met:
 #
-#         * Redistributions of source code must retain the above copyright
-#           notice, this list of conditions and the following disclaimer.
-#         * Redistributions in binary form must reproduce the above copyright
-#           notice, this list of conditions and the following disclaimer in
-#           the documentation and/or other materials provided with the
-#           distribution
+#       * Redistributions of source code must retain the above copyright
+#         notice, this list of conditions and the following disclaimer.
+#       * Redistributions in binary form must reproduce the above copyright
+#         notice, this list of conditions and the following disclaimer in
+#         the documentation and/or other materials provided with the
+#         distribution
 #
-#     THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-#     AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-#     IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
-#     ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
-#     LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
-#     CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
-#     SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
-#     INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
-#     CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
-#     ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
-#     POSSIBILITY OF SUCH DAMAGE.
-# ==============================================================================
+#   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+#   IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+#   TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A
+#   PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER
+#   OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+#   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+#   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
+#   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+#   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+#   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+#   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+# ============================================================================
 #
 # Further modifications by author J. L. Lanfranchi:
 #
 # 2015-02-24: Faster via quad -> double precision, more numpy vectorized
 #   functions, numexpr for a couple of the slower evaluations. Note that the
-#   double precision may make this fail in some circumstances, but I haven't seen
-#   it do so yet. Regardless, modifying the calls to float64 -> float128 and
-#   eliminating the numexpr calls (only supports doubles) should make it
+#   double precision may make this fail in some circumstances, but I haven't
+#   seen it do so yet. Regardless, modifying the calls to float64 -> float128
+#   and eliminating the numexpr calls (only supports doubles) should make it
 #   equivalent to the original implementation.
 #
 # 2015-03-09: Add variable-bandwidth implementation that does the following:
@@ -66,16 +60,23 @@
 #      peak-density point to use the ISJ BW found in step (1). This is done by
 #      dividing the inverse-square-root bandwidths by the bandwidth at the
 #      peak-density point and multiplying by the ISJ BW. (This appears to do
-#      quite well at both capturing the peak's characteristics and smoothing out
-#      bumps in the tails, but we should be cautious if false structures near the
-#      peak may arise due to densities similar to that of the peak.)
+#      quite well at both capturing the peak's characteristics and smoothing
+#      out bumps in the tails, but we should be cautious if false structures
+#      near the peak may arise due to densities similar to that of the peak.)
 #
 # 2015-03-28:
-#   * Removed numexpr pieces to make an as-universal-as-possible implementation,
-#     instead using far more optimized gaussian computation routines in a
-#     separate Cython .pyx file if the user can compile the Cython (depends upon
-#     OpenMP and Cython).
+#   * Removed numexpr pieces to make an as-universal-as-possible
+#     implementation, instead using far more optimized gaussian computation
+#     routines in a separate Cython .pyx file if the user can compile the
+#     Cython (depends upon OpenMP and Cython).
 #
+"""
+An implementation of the kde bandwidth selection method outlined in:
+
+Z. I. Botev, J. F. Grotowski, and D. P. Kroese. Kernel density
+estimation via diffusion. The Annals of Statistics, 38(5):2916-2957, 2010.
+"""
+
 
 from __future__ import division
 
@@ -98,7 +99,8 @@ try:
 except:
     def gaussian(outbuf, x, mu, sigma):
         xlessmu = x-mu
-        outbuf += 1./(sqrt2pi*sigma) * np.exp(-xlessmu*xlessmu/(2.*sigma*sigma))
+        outbuf += 1./(sqrt2pi*sigma) * \
+                np.exp(-xlessmu*xlessmu/(2.*sigma*sigma))
     def gaussians(outbuf, x, mu, sigma, **kwargs):
         [gaussian(outbuf, x, mu[n], sigma[n]) for n in xrange(len(mu))]
 else:
@@ -169,72 +171,82 @@ def fbw_kde(data, N=None, MIN=None, MAX=None, overfit_factor=1.0):
 
 def vbw_kde(data, N=None, MIN=None, MAX=None, evaluate_dens=True,
             evaluate_at=None, overfit_factor=1.0):
-    '''
+    """
     Parameters
     ----------
-    data            The data points for which the density estimate is sought
+    data
+        The data points for which the density estimate is sought
 
-    N               Number of points with which to form regular mesh, from MIN
-                      to MAX; this gets DCT'd, so N should be a power of two.
-                      -> Default: 2**14 (16384)
+    N
+        Number of points with which to form regular mesh, from MIN to MAX;
+        this gets DCT'd, so N should be a power of two.
+        -> Default: 2**14 (16384)
 
-    MIN             Minimum of range over which to compute density.
-                      -> Default: min(data) - range(data)/10
+    MIN
+        Minimum of range over which to compute density.
+        -> Default: min(data) - range(data)/10
 
-    MAX             Maximum of range over which to compute density>
-                      -> Default: max(data) + range(data)/10
+    MAX
+        Maximum of range over which to compute density>
+        -> Default: max(data) + range(data)/10
 
-    evaluate_dens   Whether to evaluate the density either at the mesh points
-                      defined by N, MIN, and MAX, or at the points specified by
-                      the argument evaluate_at. If False, only the gaussians'
-                      bandwidths and the mesh locations (no density) are
-                      returned. Evaluating the density is a large fraction of
-                      total execution time, so setting this to False saves time
-                      if only the bandwidths are desired.
-                      -> Default: True
+    evaluate_dens
+        Whether to evaluate the density either at the mesh points defined by
+        N, MIN, and MAX, or at the points specified by the argument
+        evaluate_at. If False, only the gaussians' bandwidths and the mesh
+        locations (no density) are returned. Evaluating the density is a large
+        fraction of total execution time, so setting this to False saves time
+        if only the bandwidths are desired.
+        -> Default: True
 
-    evaluate_at     Points at which to evaluate the density. If None is
-                      specified, evaluates at points on the mesh defined by
-                      MIN, MAX, and N.
-                      -> Default: None
+    evaluate_at
+        Points at which to evaluate the density. If None is specified,
+        evaluates at points on the mesh defined by MIN, MAX, and N.
+        -> Default: None
 
-    overfit_factor  EXPERIMENTAL: For the first part of the algorithm, the
-                      improved-Sheather-Jones fixed-bandwidth (ISJ-FBW) bit,
-                      the density can be overfit by specifying overfit_factor >
-                      1.0 and underfit using a value < 1.0.
-                      -> Default: 1.0
+    overfit_factor
+        EXPERIMENTAL: For the first part of the algorithm, the
+        improved-Sheather-Jones fixed-bandwidth (ISJ-FBW) bit, the density can
+        be overfit by specifying overfit_factor > 1.0 and underfit using a
+        value < 1.0.
+        -> Default: 1.0
 
     Returns
     -------
-    kernel_bandwidths The gaussian bandwidths, one for each data point
+    kernel_bandwidths
+        The gaussian bandwidths, one for each data point
 
-    evaluate_at       Locations at which the density is evaluated
+    evaluate_at
+        Locations at which the density is evaluated
 
-    vbw_dens_est      Density estimates at the mesh points, or None if
-                        evaluate_dens is False
+    vbw_dens_est
+        Density estimates at the mesh points, or None if evaluate_dens is
+        False
 
     Notes
     -----
     Specifying the range:
 
         The specification of MIN and MAX are critical for obtaining a
-    reasonable density estimate. If the true underlying density slowly decays
-    to zero on one side or the other, like a gaussian, specifying too-small a
-    range will distort the edge the VBW-KDE finds. On the other hand, an abrupt
-    cut-off in the distribution should be accompanied by a similar cutoff in
-    the computational range (MIN and/or MAX). The algorithm here will
-    approximate such a sharp cut-off with roughly the same performance to the
-    reflection method for standard KDE's (as the fixed-BW portion uses a DCT of
-    the data), but note that this will not perform as well as polynomial-edges
-    or other modifications that have been proposed in the literature.
+        reasonable density estimate. If the true underlying density slowly
+        decays to zero on one side or the other, like a gaussian, specifying
+        too-small a range will distort the edge the VBW-KDE finds. On the
+        other hand, an abrupt cut-off in the distribution should be
+        accompanied by a similar cutoff in the computational range (MIN and/or
+        MAX). The algorithm here will approximate such a sharp cut-off with
+        roughly the same performance to the reflection method for standard
+        KDE's (as the fixed-BW portion uses a DCT of the data), but note that
+        this will not perform as well as polynomial-edges or other
+        modifications that have been proposed in the literature.
 
     Specifying overfit_factor; other tweaks:
 
-        I've seen no improvement by changing this parameter, but it remains for
-    experimental purposes. Other avenues to explore include changing the
-    "normalization" of the variable-bandwidth bit that I use which forces it to
-    have a bandwidth at the peak matching that found by the ISJ-FBW part
-    '''
+        I've seen no improvement by changing this parameter, but it remains
+        for experimental purposes. Other avenues to explore include changing
+        the "normalization" of the variable-bandwidth bit that I use which
+        forces it to have a bandwidth at the peak matching that found by the
+        ISJ-FBW part
+    """
     # Parameters to set up the mesh on which to calculate
     if N is None:
         N = 2**14 #if N is None else int(2**np.ceil(np.log2(N)))
