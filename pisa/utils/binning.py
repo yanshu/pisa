new object decorator for binning, fixing units copy problem

pisa/utils/binning.py
@@ -39,6 +39,20 @@ class OneDimBinning(object):
         ('domain', '%s_domain'),
         ('n_bins', 'n_%sbins'),
     )
+    def new_obj(original_function):
+        """ decorator to deepcopy unaltered states into new object """
+        def new_function(self, *args, **kwargs):
+            state = OrderedDict()
+            dict = original_function(self, *args, **kwargs)
+            for slot in self.__state_attrs:
+                if dict.has_key(slot):
+                    state[slot] = dict[slot]
+                elif slot == 'units':
+                    state[slot] = copy(self.__getattr__(slot))
+                else:
+                    state[slot] = deepcopy(self.__getattr__(slot))
+            return OneDimBinning(**state)
+        return new_function
     """
     Histogram-oriented binning specialized to a single dimension.
 
@@ -162,6 +176,8 @@ class OneDimBinning(object):
         #for attr in
         #self.n_bins =
 
+    #def __deepcopy__(self,memo): return copy(self)
+
     @staticmethod
     def is_bin_spacing_log(bin_edges):
         """Check if `bin_edges` define a logarithmically-uniform bin spacing.
@@ -274,6 +290,7 @@ class OneDimBinning(object):
             return True
         return False
 
+    @new_obj
     def oversample(self, factor):
         """Return a OneDimBinning object oversampled relative to this object's
         binning.
@@ -290,7 +307,6 @@ class OneDimBinning(object):
         """
         assert factor >= 1 and factor == int(factor)
         factor = int(factor)
-        new_state = deepcopy(self.state)
         if self.is_log:
             bin_edges = np.logspace(np.log10(self.domain[0]),
                                     np.log10(self.domain[-1]),
@@ -307,8 +323,10 @@ class OneDimBinning(object):
                 bin_edges.extend(this_bin_new_edges[:-1])
             # Final bin needs final edge
             bin_edges.append(this_bin_new_edges[-1])
-        new_state['bin_edges'] = bin_edges
-        return OneDimBinning(**new_state)
+        return{'bin_edges':bin_edges}
+
+    def __getattr__(self, attr):
+        return super(OneDimBinning, self).__getattribute__(attr)
 
     @property
     def state(self):
@@ -352,6 +370,7 @@ class OneDimBinning(object):
     def __repr__(self):
         return str(self)
 
+    @new_obj
     def __getitem__(self, index):
         """Return a new OneDimBinning, sub-selected by `index`.
 
@@ -386,9 +405,7 @@ class OneDimBinning(object):
         bin_edges.append(self.bin_edges[final_bin_index])
 
         # Retrieve current state; only bin_edges needs to be updated
-        new_state = deepcopy(self.state)
-        new_state['bin_edges'] = bin_edges
-        return OneDimBinning(**new_state)
+        return {'bin_edges':bin_edges}
 
 class MultiDimBinning(object):
     """
@@ -557,33 +574,24 @@ class MultiDimBinning(object):
             raise ValueError('Binning is %dD, but %dD indexing was passed'
                              % (self.n_dims, input_dim))
         new_binning = []
+        print index
+        print self.dimensions
         for dim, idx in zip(self.dimensions, index):
+            print dim.bin_edges
             new_binning.append(dim[idx])
         return MultiDimBinning(*new_binning)
 
-
-def e_binning(emin, emax, n_ebins, is_log=True):
-    return OneDimBinning(name='energy', units='GeV', prefix='e', tex=r'E_\nu',
-                         is_log=is_log, n_bins=n_ebins, domain=(emin, emax))
-
-
-def cz_binning(czmin, czmax, n_czbins, is_lin=True):
-    return OneDimBinning(name='coszen', units=None, prefix='cz',
-                         tex=r'\cos\,\theta', is_lin=is_lin,
-                         n_bins=n_czbins, domain=(czmin, czmax))
-
-
 def test_Binning():
-    b1 = Binning(e_range=[1,80], cz_range=[-1,0], n_ebins=40, n_czbins=20,
-                 e_is_log=True)
-    b2 = Binning(ebins=np.logspace(0, np.log10(80), 41),
-                 czbins=np.linspace(-1, 0, 21), e_is_log=True)
+    b1 = OneDimBinning(name='energy',units=units.GeV, prefix='e', n_bins=40, is_log=True, domain=[1,80])
+    b2 = OneDimBinning(name='coszen',units=units.dimensionless, prefix='cz',
+            n_bins=40, is_lin=True, domain=[-1,1])
     print 'b1:', b1
     print 'b2:', b2
-    assert b1 == b2
-    print 'b1[0:5, 0:5]:', b1[0:5, 0:5]
-    assert b1[0:40, 0:20] == b1
+    b1.oversample(10)
+    print 'b1[0:5, 0:5]:', b1[0:5]
 
 
 if __name__ == "__main__":
+    import pint
+    units = pint.UnitRegistry() 
     test_Binning()