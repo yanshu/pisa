f98c10e78b3839b036c425b7d6ae9337767c441b
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-05-02 02:23:44 -0400

    Extra init parameters moved into `params`; converted to Stage base class (untested)

--- pisa/stages/flux/dummy.py
+++ pisa/stages/flux/dummy.py
@@ -1,28 +1,36 @@
-from pisa.core.stage import NoInputStage
+from pisa.core.stage import Stage
 from pisa.core.map import Map, MapSet
 import numpy as np
 import pint
 units = pint.UnitRegistry()
 
-class dummy(NoInputStage):
+class dummy(Stage):
     """
     This is a Flux Service just for testing purposes, generating a random map
     m1 and a map containing ones as m2 a parameter test is required
     """
-    def __init__(self, params, example_file, output_binning, 
-                service, oversample_e=1, oversample_cz=1):
+    def __init__(self, params, output_binning, disk_cache=None,
+                 memcaching_enabled=True, propagate_errors=True):
         # list expected parameters for this stage implementation
-        expected_params = ['atm_delta_index', 'energy_scale', 'nu_nubar_ratio',
-                            'nue_numu_ratio', 'test']
+        expected_params = (
+            'atm_delta_index', 'energy_scale', 'nu_nubar_ratio',
+            'nue_numu_ratio', 'test', 'example_file', 'oversample_e',
+            'oversample_cz'
+        )
         # call parent constructor
-        super(self.__class__, self).__init__(stage_name='flux',
-                                             service_name=service,
-                params=params, expected_params=expected_params)
+        super(self.__class__, self).__init__(
+            input_stage=False, stage_name='flux', service_name='dummy',
+            params=params, expected_params=expected_params,
+            disk_cache=disk_cache, memcaching_enabled=memcaching_enabled,
+            propagate_errors=propagate_errors,
+            results_cache_depth=results_cache_depth,
+            output_binning=output_binning
+        )
         # asign other attributes
-        self.filename = example_file
-        self.output_binning = output_binning
-        self.oversample_e = oversample_e
-        self.oversample_cz = oversample_cz
+        #self.filename = example_file
+        #self.output_binning = output_binning
+        #self.oversample_e = oversample_e
+        #self.oversample_cz = oversample_cz
 
     def _derive_output(self):
         outputs = ['nue', 'numu', 'nuebar', 'numubar']
@@ -36,7 +44,7 @@ class dummy(NoInputStage):
             m = Map(output, hist, self.output_binning)
             m.set_poisson_errors()
             output_maps.append(m)
-        mapset = MapSet(maps=output_maps, name = 'flux maps')
+        mapset = MapSet(maps=output_maps, name='flux maps')
         return mapset
 
     def validate_params(self, params):
