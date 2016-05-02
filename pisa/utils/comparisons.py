00a0b814a1890654d6b21f7bf4948a6ca00c5d8d
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-05-02 02:19:18 -0400

    Handle pint units, and more intelligent handling of pint quantities (convert values to same units before comparing magnitudes); unify string and "simple" object comparisons into one. Add logging.trace for all comparisons that evaluate to False for debugging.

--- pisa/utils/comparisons.py
+++ pisa/utils/comparisons.py
@@ -1,56 +1,97 @@
 
 import sys
-from collections import Mapping, Sequence, OrderedDict
 import itertools
+from collections import Mapping, Sequence, OrderedDict
 
 import numpy as np
+import pint
 
+from pisa.utils.log import logging, set_verbosity
+
+set_verbosity(10)
 
 NP_TYPES = (np.ndarray, np.matrix)
 SEQ_TYPES = (Sequence, np.ndarray, np.matrix)
 MAP_TYPES = (Mapping,)
 COMPLEX_TYPES = tuple(list(NP_TYPES) + list(SEQ_TYPES) + list(MAP_TYPES))
 
+def is_pint_quantity(x):
+    #return isinstance(x, pint.quantity._Quantity)
+    return hasattr(x, 'units') and hasattr(x, 'magnitude')
 
 def recursiveEquality(x, y):
     """Recursively verify equality between two objects x and y."""
     # NOTE: The order in which types are compared matters.
 
-    # pint unit
-    if hasattr(x, 'units') and hasattr(x, 'magnitude'):
-        return (x.u == y.u) and recursiveEquality(x.m, y.m)
+    # pint units
+    if isinstance(x, pint.unit._Unit) and isinstance(y, pint.unit._Unit):
+        if x.dimensionality != y.dimensionality:
+            logging.trace('x.dimensionality: %s' %x.dimensionality)
+            logging.trace('y.dimensionality: %s' %y.dimensionality)
+            return False
 
-    # string
-    elif isinstance(x, str):
-        return x == y
+    # pint quantities
+    if is_pint_quantity(x) and is_pint_quantity(y):
+        xunit = str(x.units)
+        try:
+            converted_y = y.to(xunit)
+        except DimensionalityError:
+            logging.trace('Incompatible units: x.units=%s, y.units=%s'
+                          %(x.units, y.units))
+            return False
+        # Check for equality to double precision
+        if not np.allclose(x.magnitude, converted_y.magnitude,
+                           rtol=1e-15, atol=0):
+            logging.trace('x.magnitude: %s' %x.magnitude)
+            logging.trace('y.magnitude: %s' %y.magnitude)
+            return False
 
-    elif not (isinstance(x, COMPLEX_TYPES) or isinstance(y, COMPLEX_TYPES)):
-        return x == y
+    # simple things can be compared directly
+    elif isinstance(x, basestring) or isinstance(y, basestring) or \
+            (not (isinstance(x, COMPLEX_TYPES) or
+                  isinstance(y, COMPLEX_TYPES))):
+        if x != y:
+            logging.trace('Simple types (type(x)=%s, type(y)=%s) not equal.'
+                          %(type(x), type(y)))
+            logging.trace('x: %s' %x)
+            logging.trace('y: %s' %y)
+            return False
 
     # Numpy types
     elif isinstance(x, NP_TYPES) or isinstance(y, NP_TYPES):
         if np.shape(x) != np.shape(y):
+            logging.trace('shape(x): %s' %np.shape(x))
+            logging.trace('shape(y): %s' %np.shape(y))
             return False
         if not np.all(np.equal(x, y)):
+            logging.trace('x: %s' %x)
+            logging.trace('y: %s' %y)
             return False
 
     # Dict
     elif isinstance(x, Mapping):
         xkeys = sorted(x.keys())
         if not xkeys == sorted(y.keys()):
+            logging.trace('xkeys: %s' %(xkeys,))
+            logging.trace('ykeys: %s' %(sorted(y.keys()),))
             return False
         else:
             for k in xkeys:
                 if not recursiveEquality(x[k], y[k]):
+                    logging.trace('not equal found at key: "%s"' %k)
                     return False
 
     # Non-numpy sequence
     elif isinstance(x, Sequence):
         if not len(x) == len(y):
+            logging.trace('len(x): %s' %len(x))
+            logging.trace('len(y): %s' %len(y))
             return False
         else:
             for xs, ys in itertools.izip(x, y):
                 if not recursiveEquality(xs, ys):
+                    logging.trace('xs: %s' %xs)
+                    logging.trace('ys: %s' %ys)
                     return False
 
     # Unhandled
@@ -58,6 +99,8 @@ def recursiveEquality(x, y):
         raise TypeError('Unhandled type(s): %s, x=%s, y=%s' %
                         (type(x), str(x), str(y)))
 
+    # Returns above only occur if comparisons evaluate to False; therefore, if
+    # you make it here, everything is equal.
     return True
 
 
@@ -159,8 +202,18 @@ def test_recursiveEquality():
     assert not recursiveEquality(d4, d6)
     assert recursiveEquality(d7, d8)
 
+    ureg0 = pint.UnitRegistry()
+    ureg1 = pint.UnitRegistry()
+    u0 = ureg0.GeV
+    u1 = ureg1.MeV
+    q0 = np.ones(100) * u0
+    q1 = np.ones(100) * 1000.0 * u1
+    assert recursiveEquality(q0, q1)
+    assert not recursiveEquality(u0, u1)
+
     print '<< PASSED >> recursiveEquality'
 
 
 if __name__ == "__main__":
+    set_verbosity(10)
     test_recursiveEquality()
