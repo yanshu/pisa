10e8963a42666e73296e811ec4e879e8f9b561c8
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-05-02 05:35:38 -0400

    Attempting more detail in Stage subclass templates (examples)

--- pisa/stages/inputstage_template.py
+++ pisa/stages/inputstage_template.py
@@ -1,15 +1,44 @@
-from pisa.stage import InputStage
+from pisa.core.stage import Stage
 
-class Flux(InputStage):
+class service_name(Stage):
     """
     Docstring here
+
+    Parameters
+    ----------
+    params
+    input_binning
+    output_binning
+    disk_cache
+    transforms_cache_depth
+    outputs_cache_depth
+
     """
-    def _derive_transform(self, **kwargs):
+    def __init__(self, params, input_binning, output_binning, disk_cache,
+                 transforms_cache_depth=20, outputs_cache_depth=20):
+        expected_params = (
+            'oversample_e', 'oversample_cz', 'earth_model',
+            'YeI', 'YeM', 'YeO', 'deltacp', 'deltam21', 'deltam31',
+            'detector_depth', 'prop_height', 'test', 'theta12', 'theta13',
+            'theta23'
+        )
+        super(self.__class__, self).__init__(
+            use_transforms=True,
+            stage_name='...',
+            service_name='service_name',
+            params=params,
+            expected_params=expected_params,
+            disk_cache=disk_cache,
+            outputs_cache_depth=outputs_cache_depth,
+            transforms_cache_depth=transforms_cache_depth,
+            input_binning=input_binning,
+            output_binning=output_binning
+        )
+
+    def _compute_transforms(self):
+        """Docstring here"""
         pass
 
     def validate_params(self, params):
-        pass
-
-    @staticmethod
-    def add_cmdline_args(parser):
+        """Docstring here"""
         pass
--- pisa/stages/noinputstage_template.py
+++ pisa/stages/noinputstage_template.py
@@ -1,9 +1,31 @@
-from pisa.stage import NoInputStage
+from pisa.core.stage import Stage
 
-class Flux(NoInputStage):
+class service_name(Stage):
     """
     Docstring here
     """
+    def __init__(self, params, output_binning, disk_cache=None,
+                 memcaching_enabled=True, propagate_errors=True,
+                 outputs_cache_depth=20):
+        # list expected parameters for this stage implementation
+        expected_params = (
+            'atm_delta_index', 'energy_scale', 'nu_nubar_ratio',
+            'nue_numu_ratio', 'test', 'example_file', 'oversample_e',
+            'oversample_cz'
+        )
+        # call parent constructor
+        super(self.__class__, self).__init__(
+            use_transforms=False,
+            stage_name='flux',
+            service_name='service_name',
+            params=params,
+            expected_params=expected_params,
+            disk_cache=disk_cache,
+            memcaching_enabled=memcaching_enabled,
+            propagate_errors=propagate_errors,
+            outputs_cache_depth=outputs_cache_depth,
+            output_binning=output_binning
+        )
     def _derive_output(self, **kwargs):
         pass
 
