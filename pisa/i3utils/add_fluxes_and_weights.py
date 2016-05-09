f2c58d505a6f038c7d107cdeb6065abfefbf3267
yanshu <huangfeifei91@gmail.com>
2016-05-09 14:11:04 -0400

    avoid overwriting output files

--- pisa/i3utils/add_fluxes_and_weights.py
+++ pisa/i3utils/add_fluxes_and_weights.py
@@ -19,30 +19,33 @@ def add_fluxes_to_file(data_file_path, file_type, phys_params, flux_service, osc
     print 'data_file_path = ', data_file_path
     if file_type == 'pisa':
         data_file, attrs = from_hdf(resources.find_resource(data_file_path), return_attrs = True)
-        for prim in data_file.keys():
-            for int_type in data_file[prim].keys():
-                true_e = data_file[prim][int_type]['true_energy']
-                true_cz = data_file[prim][int_type]['true_coszen']
-                isbar = '_bar' if 'bar' in prim else ''
-                nue_flux = flux_service.get_flux(true_e, true_cz, 'nue'+isbar, event_by_event=True)
-                numu_flux = flux_service.get_flux(true_e, true_cz, 'numu'+isbar, event_by_event=True)
-                # the opposite flavor fluxes( used only in the nu_nubar_ratio systematic)
-                oppo_isbar = '' if 'bar' else in prim '_bar'
-                oppo_nue_flux = flux_service.get_flux(true_e, true_cz, 'nue'+oppo_isbar, event_by_event=True)
-                oppo_numu_flux = flux_service.get_flux(true_e, true_cz, 'numu'+oppo_isbar, event_by_event=True)
-                data_file[prim][int_type][neutrino_weight_name+'_nue_flux'] = nue_flux
-                data_file[prim][int_type][neutrino_weight_name+'_numu_flux'] = numu_flux
-                data_file[prim][int_type][neutrino_weight_name+'_oppo_nue_flux'] = oppo_nue_flux
-                data_file[prim][int_type][neutrino_weight_name+'_oppo_numu_flux'] = oppo_numu_flux
-                # if need to calculate neutrino weights before hand
-                if add_weights:
-                    osc_probs = osc_service.fill_osc_prob(true_e, true_cz, event_by_event=True, **phys_params)
-                    osc_flux = nue_flux*osc_probs['nue'+isbar+'_maps'][prim]+ numu_flux*osc_probs['numu'+isbar+'_maps'][prim]
-                    data_file[prim][int_type][neutrino_weight_name+'_weight'] = osc_flux * data_file[prim][int_type]['weighted_aeff'] 
         data_file_name = os.path.basename(data_file_path)
         utils.mkdir(args.outdir)
         output_file_name = outdir + '/' + data_file_name.split('.hdf5')[0]+'_with_weights.hdf5' 
-        to_hdf(data_file, output_file_name, attrs=attrs, overwrite=True)
+        if not os.path.isfile(output_file_name):
+            for prim in data_file.keys():
+                for int_type in data_file[prim].keys():
+                    true_e = data_file[prim][int_type]['true_energy']
+                    true_cz = data_file[prim][int_type]['true_coszen']
+                    isbar = '_bar' if 'bar' in prim else ''
+                    nue_flux = flux_service.get_flux(true_e, true_cz, 'nue'+isbar, event_by_event=True)
+                    numu_flux = flux_service.get_flux(true_e, true_cz, 'numu'+isbar, event_by_event=True)
+                    # the opposite flavor fluxes( used only in the nu_nubar_ratio systematic)
+                    oppo_isbar = '' if 'bar' else in prim '_bar'
+                    oppo_nue_flux = flux_service.get_flux(true_e, true_cz, 'nue'+oppo_isbar, event_by_event=True)
+                    oppo_numu_flux = flux_service.get_flux(true_e, true_cz, 'numu'+oppo_isbar, event_by_event=True)
+                    data_file[prim][int_type][neutrino_weight_name+'_nue_flux'] = nue_flux
+                    data_file[prim][int_type][neutrino_weight_name+'_numu_flux'] = numu_flux
+                    data_file[prim][int_type][neutrino_weight_name+'_oppo_nue_flux'] = oppo_nue_flux
+                    data_file[prim][int_type][neutrino_weight_name+'_oppo_numu_flux'] = oppo_numu_flux
+                    # if need to calculate neutrino weights before hand
+                    if add_weights:
+                        osc_probs = osc_service.fill_osc_prob(true_e, true_cz, event_by_event=True, **phys_params)
+                        osc_flux = nue_flux*osc_probs['nue'+isbar+'_maps'][prim]+ numu_flux*osc_probs['numu'+isbar+'_maps'][prim]
+                        data_file[prim][int_type][neutrino_weight_name+'_weight'] = osc_flux * data_file[prim][int_type]['weighted_aeff'] 
+            to_hdf(data_file, output_file_name, attrs=attrs, overwrite=True)
+        else:
+            print 'File %s already exists, skipped. Please delete it or rename it.' % output_file_name
 
     elif file_type == 'intermediate':
         data_file = h5py.File(resources.find_resource(data_file_path), 'r+')
