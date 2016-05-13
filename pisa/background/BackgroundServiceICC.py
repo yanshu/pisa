9ceb8e6cbde9209003a337c77c7cdd7aaea64ba2
yanshu <huangfeifei91@gmail.com>
2016-05-12 22:52:25 -0400

    added sanity checks for background

--- pisa/background/BackgroundServiceICC.py
+++ pisa/background/BackgroundServiceICC.py
@@ -31,6 +31,17 @@ class BackgroundServiceICC:
         self.icc_bg_dict = {}
         logging.info("Creating a ICC background dict...")
 
+        # check all level cuts are correct
+        santa_doms = bg_file['IC86_Dunkman_L6_SANTA_DirectDOMs']['value']
+        l3 = bg_file['IC86_Dunkman_L3']['value']
+        l4 = bg_file['IC86_Dunkman_L4']['result']
+        l5 = bg_file['IC86_Dunkman_L5']['bdt_score']
+        assert(np.all(santa_doms>=3) and np.all(l3 == 1) and np.all(l4 == 1) and np.all(l5 >= 0.1))
+        l6 = bg_file['IC86_Dunkman_L6']
+        corridor_doms_over_threshold = l6['corridor_doms_over_threshold']
+        inverted_corridor_cut = corridor_doms_over_threshold > 1
+        assert(np.all(inverted_corridor_cut) and np.all(l6['santa_direct_doms'] >= 3) and np.all(l6['mn_start_contained'] == 1.) and np.all(l6['mn_stop_contained'] == 1.))
+
         dLLH = np.array(bg_file['IC86_Dunkman_L6']['delta_LLH'])
         if sim_ver == '4digit':
             reco_energy_all = np.array(bg_file['IC86_Dunkman_L6_MultiNest8D_PDG_Neutrino']['energy'])
@@ -44,9 +55,9 @@ class BackgroundServiceICC:
             reco_energy_all = np.array(bg_file['IC86_Dunkman_L6_PegLeg_MultiNest8D_NumuCC']['energy'])
             reco_coszen_all = np.array(np.cos(bg_file['IC86_Dunkman_L6_PegLeg_MultiNest8D_NumuCC']['zenith']))
             # throw away delta LLH < -2:
-            reco_energy_all = reco_energy_all[dLLH>=-2]
-            reco_coszen_all = reco_coszen_all[dLLH>=-2]
-            dLLH = dLLH[dLLH>=-2]
+            #reco_energy_all = reco_energy_all[dLLH>=-2]
+            #reco_coszen_all = reco_coszen_all[dLLH>=-2]
+            #dLLH = dLLH[dLLH>=-2]
             pid_cut = 3.0       # this cut value 3.0 is from MSU, might need to be optimized
         else:
             raise ValueError('Only allow sim_ver  4digit or 5 digit!')
