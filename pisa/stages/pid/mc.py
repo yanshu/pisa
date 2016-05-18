7992185102c40613f4c9e949a0aaaa7645acb46d
shivesh <s.p.mandalia@qmul.ac.uk>
2016-05-18 14:46:27 +0100

    Port basic PID Service from PISAv2

--- pisa/stages/pid/mc.py
+++ pisa/stages/pid/mc.py
@@ -1,4 +1,4 @@
-
+#
 # PISA authors: Lukas Schulte
 #               schulte@physik.uni-bonn.de
 #               Justin L. Lanfranchi
@@ -28,7 +28,7 @@ import numpy as np
 from pisa.core.stage import Stage
 from pisa.core.transform import BinnedTensorTransform, TransformSet
 from pisa.utils.events import Events
-from pisa.utils.flavInt import NuFlavInt
+from pisa.utils.flavInt import NuFlavInt, NuFlavIntGroup
 from pisa.utils.PIDSpec import PIDSpec
 from pisa.utils.dataProcParams import DataProcParams
 from pisa.utils.hash import hash_obj
@@ -45,6 +45,7 @@ class mc(Stage):
 
     Parameters
     ----------
+    # TODO(shivesh): Correctly classify the params
     params : ParamSet or sequence with which to instantiate a ParamSet
         Parameters which set everything besides the binning.
 
@@ -173,7 +174,7 @@ class mc(Stage):
 
         # TODO(shivesh): check is error handled is managed by Transform
         #self.error_computed = False
-        events = Events(self.params['pid_events'])
+        events = Events(self.params['pid_events'].value)
 
         # TODO(shivesh): figure out what this comment means
         # Select only the inits in the input/output binning for conversion
@@ -207,7 +208,7 @@ class mc(Stage):
             proc_ver=events.metadata['proc_ver']
         )
 
-        if self.params['pid_remove_true_downgoing']:
+        if self.params['pid_remove_true_downgoing'].value:
             # TODO(shivesh): more options for cuts?
             cut_events = data_proc_params.applyCuts(
                 events, cuts='true_upgoing_coszen'
@@ -216,60 +217,115 @@ class mc(Stage):
             cut_events = events
 
         pid_spec = PIDSpec(
-            detector=cut_events.metadata['detector'],
-            geom=cut_events.metadata['geom'],
-            proc_ver=cut_events.metadata['proc_ver'],
-            pid_specs=self.params['pid_spec_source']
+            detector=events.metadata['detector'],
+            geom=events.metadata['geom'],
+            proc_ver=events.metadata['proc_ver'],
+            pid_specs=self.params['pid_spec_source'].value
         )
-        if self.output_names != pid_spec.get_signatures():
-            msg = 'PID criteria from `pid_spec` ({0}) does not \
-                    match the following specification {1}'
+        u_out_names = map(unicode, self.output_names)
+        if set(u_out_names) != set(pid_spec.get_signatures()):
+            msg = 'PID criteria from `pid_spec` {0} does not match {1}'
             raise ValueError(msg.format(pid_spec.get_signatures(),
-                                        self.output_names))
+                                        u_out_names))
 
         # TODO: add importance weights, error computation
 
         logging.info("Separating events by PID...")
         separated_events = pid_spec.applyPID(
             events=cut_events,
-            return_fields=['reco_energy', 'reco_coszen']
+            return_fields=['reco_energy', 'reco_coszen', 'weighted_aeff']
         )
 
         # These get used in innermost loop, so produce it just once here
         all_bin_edges = [edges.magnitude for edges in output_binning.bin_edges]
-        rep_flavint = flavInt.NuFlavIntGroup(self.input_names)
 
         transforms = []
-        for signature in self.output_names:
+        for sig in self.output_names:
+            logging.debug('Working on {0} particle ID'.format(sig))
+            # TODO(shivesh): error propagation
+            raw_histo = {}
+            total_histo = np.zeros(output_binning.shape)
 
+            # TODO(shivesh): total histo check?
+            for flavint in self.input_names:
+                raw_histo[flavint] = {}
+                rep_flavint = NuFlavIntGroup(flavint)[0]
+                flav_sigdata = separated_events[rep_flavint][sig]
+                reco_e = flav_sigdata['reco_energy']
+                reco_cz = flav_sigdata['reco_coszen']
+                weights = flav_sigdata['weighted_aeff']
+                raw_histo[rep_flavint], _, _ = np.histogram2d(
+                    reco_e,
+                    reco_cz,
+                    weights=weights,
+                    bins=all_bin_edges,
+                )
+                total_histo += raw_histo[rep_flavint]
+
+            for flavint in self.input_names:
+                rep_flavint = NuFlavIntGroup(flavint)[0]
+                xform_array = raw_histo[rep_flavint]/ total_histo
+
+                invalid_idx = total_histo == 0
+                valid_idx = 1-invalid_idx
+                invalid_idx = np.where(invalid_idx)[0]
+                num_invalid = len(invalid_idx)
+
+                message = 'Group "%s", PID signature "%s" has %d invalid' \
+                        ' entry(ies)!' % (flavint, sig, num_invalid)
+
+                if num_invalid > 0 and not self.params['replace_invalid']:
+                    pass
+                    #raise ValueError(message)
+
+                replace_idx = []
+                if num_invalid > 0 and self.params['replace_invalid']:
+                    logging.warn(message)
+                    valid_idx = np.where(valid_idx)[0]
+                    for idx in invalid_idx:
+                        dist = np.abs(valid_idx-idx)
+                        nearest_valid_idx = valid_idx[np.where(dist==np.min(dist))[0][0]]
+                        replace_idx.append(nearest_valid_idx)
+                        xform_array[idx] = xform_array[nearest_valid_idx]
+
+                xform = BinnedTensorTransform(
+                    input_names=flavint,
+                    output_name=sig,
+                    input_binning=input_binning,
+                    output_binning=self.output_binning,
+                    xform_array=xform_array
+                )
+                transforms.append(xform)
+
+        return TransformSet(transforms=transforms)
 
     def validate_params(self, params):
         # do some checks on the parameters
 
         # Check type of pid_events
-        assert isinstance(params['pid_events'], (basestring, Events))
+        # TODO(shivesh): assertion for units
+        assert isinstance(params['pid_events'].value, (basestring, Events))
 
         # Check type of compute_error, replace_invalid,
         # pid_remove_true_downgoing
-        assert isinstance(params['compute_error'], bool)
-        assert isinstance(params['replace_invalid'], bool)
-        assert isinstance(params['pid_remove_true_downgoing'], bool)
+        assert isinstance(params['compute_error'].value, bool)
+        assert isinstance(params['replace_invalid'].value, bool)
+        assert isinstance(params['pid_remove_true_downgoing'].value, bool)
 
         # Check type of pid_ver, pid_spec_source
-        assert isinstance(params['pid_ver'], basestring)
-        assert isinstance(params['pid_spec_source'], basestring)
+        assert isinstance(params['pid_ver'].value, basestring)
+        assert isinstance(params['pid_spec_source'].value, basestring)
 
         # Check the groupings of the pid_events file
-        # TODO(shivesh): check the events initialisation
-        events = Events(params['pid_events'])
+        events = Events(params['pid_events'].value)
         should_be_joined = sorted([
-            flavInt.NuFlavIntGroup('nuecc+nuebarcc'),
-            flavInt.NuFlavIntGroup('numucc+numubarcc'),
-            flavInt.NuFlavIntGroup('nutaucc+nutaubarcc'),
-            flavInt.NuFlavIntGroup('nuallnc+nuallbarnc'),
+            NuFlavIntGroup('nuecc+nuebarcc'),
+            NuFlavIntGroup('numucc+numubarcc'),
+            NuFlavIntGroup('nutaucc+nutaubarcc'),
+            NuFlavIntGroup('nuallnc+nuallbarnc'),
         ])
         are_joined = sorted([
-            flavInt.NuFlavIntGroup(s)
+            NuFlavIntGroup(s)
             for s in events.metadata['flavints_joined']
         ])
         if are_joined != should_be_joined:
