558297ae4a3fa568fe3791a80a13af285236058c
philippeller <peller@phys.ethz.ch>
2016-05-24 14:00:09 -0400

    parse bool

--- pisa/utils/parse_config.py
+++ pisa/utils/parse_config.py
@@ -53,6 +53,8 @@ def parse_quantity(string):
 
 
 def parse_string_literal(string):
+    if string.lower().strip() == 'true': return True
+    elif string.lower().strip() == 'false' return False 
     return string
 
 
