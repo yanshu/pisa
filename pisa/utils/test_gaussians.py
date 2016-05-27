7861189b75e4703cd7f7c1d14b334d01d787e585
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-05-27 12:26:33 -0400

    Add speed_test_gaussians() function and command-line invocation

--- pisa/utils/test_gaussians.py
+++ pisa/utils/test_gaussians.py
@@ -61,7 +61,7 @@ def test_gaussians():
 
     # Compute the reference result
     [refbuf.__iadd__(norm.pdf(x, loc=m, scale=s)) for m, s in izip(mu, sigma)]
-    
+
     # Try out the threads functionality for each result; reset the accumulation
     # buffer each time.
     for threads in (1, 2, 32):
@@ -72,6 +72,57 @@ def test_gaussians():
                 %(outbuf, refbuf, mu, sigma, threads)
 
 
+def speed_test_gaussians():
+    import multiprocessing
+    import time
+    import sys
+
+    def wstdout(msg):
+        sys.stdout.write(msg)
+        sys.stdout.flush()
+
+    num_cpu = multiprocessing.cpu_count()
+
+    np.random.seed(0)
+    mu = np.array(np.random.randn(1e4), dtype=np.float64)
+    sigma = np.array(np.abs(np.random.randn(len(mu))), dtype=np.float64)
+    np.clip(sigma, a_min=1e-20, a_max=np.inf, out=sigma)
+
+    x = np.linspace(-10, 10, 1e4, dtype=np.float64)
+
+    # Place to store result of `gaussians()`; zero-stuffed in the below lopp
+    outbuf = np.empty_like(x, dtype=np.float64)
+
+    # Place to store result of `scipy.stats.norm`
+    refbuf = np.zeros_like(outbuf, dtype=np.float64)
+
+    # Compute the reference result
+    [refbuf.__iadd__(norm.pdf(x, loc=m, scale=s)) for m, s in izip(mu, sigma)]
+
+    # Try out the threads functionality for each result; reset the accumulation
+    # buffer each time.
+    timings = []
+    wstdout('%7s %10s\n' %('Threads', 'Time'))
+    for threads in range(1, num_cpu+1):
+        outbuf.fill(0)
+        t0 = time.time()
+        gaussians(outbuf, x, mu, sigma, threads)
+        timing = time.time() - t0
+        timings.append((threads, timing))
+        assert np.allclose(outbuf, refbuf, rtol=1e-14, atol=0, equal_nan=True),\
+                'outbuf=\n%s\nrefbuf=\n%s\nmu=\n%s\nsigma=\n%s\nthreads=%d' \
+                %(outbuf, refbuf, mu, sigma, threads)
+        wstdout('%7d %10.3e\n' %(threads, timing))
+
+
 if __name__ == '__main__':
-    test_gaussian()
-    test_gaussians()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-s', '--speed', action='store_true',
+                        help='Run speed test rather than unit tests')
+    args = parser.parse_args()
+    if args.speed:
+        speed_test_gaussians()
+    else:
+        test_gaussian()
+        test_gaussians()
