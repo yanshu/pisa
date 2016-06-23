c34a119733bbd8bda32fdba7c9c3e4bd3d2507ca
Philipp Eller <peller@phys.ethz.ch>
2016-06-23 12:58:54 -0400

    generalizing...

--- nutau/slopes.py
+++ nutau/slopes.py
@@ -23,8 +23,20 @@ parser.add_argument('-v', action='count', default=None,
 args = parser.parse_args()
 set_verbosity(args.v)
 
+# 1: linear, 2: quadratic, etc...
 degree = 2
-force_through_nominal = False
+# force fit to go through nominal point
+force_through_nominal = True
+
+# define fit function
+if force_through_nominal:
+    function = "fit_fun = lambda x, *p: polyval(x, [1.] + list(p))"
+else:
+    function = "fit_fun = lambda x, *p: polyval(x, list(p))"
+    # add free parameter for constant term
+    degree += 1
+
+exec(function)
 
 if args.plot: 
     import matplotlib.pyplot as plt
@@ -40,17 +52,23 @@ fname_joined = 'joined_G_nue_cc+nuebar_cc_G_numu_cc+numubar_cc_G_nutau_cc+nutaub
 
 pname = 'dom_eff'
 nominal = 1.0
-runs = [('601', 0.88), ('603', 0.94), ('604', 0.97), ('605', 1.03), ('606', 1.06), ('608', 1.12)]
+#runs = [('601', 0.88), ('603', 0.94), ('604', 0.97), ('605', 1.03), ('606', 1.06), ('608', 1.12)]
+runs = [('601', 0.88), ('603', 0.94), ('606', 1.06), ('608', 1.12)]
 
 x_values = np.array(sorted([r[1] for r in runs] + [nominal]))
 
-cscd = {}
-trck = {}
+categories = ['cscd', 'trck']
+plt_colors = {'cscd':'b', 'trck':'r'}
+
+inputs = {}
+for cat in categories:
+     inputs[cat] = {}
 
 # get templates
 template = template_maker.get_outputs()
-cscd[nominal] = sum([map.hist for map in template if map.name.endswith('cscd')])
-trck[nominal] = sum([map.hist for map in template if map.name.endswith('trck')])
+for cat in categories:
+    inputs[cat][nominal] = sum([map.hist for map in template if
+            map.name.endswith(cat)])
 
 for run, value in runs:
     # adjust params
@@ -68,20 +86,22 @@ for run, value in runs:
     template_maker.update_params(param)
     # retreive maps
     template = template_maker.get_outputs()
-    cscd[value] = sum([map.hist for map in template if map.name.endswith('cscd')])
-    trck[value] = sum([map.hist for map in template if map.name.endswith('trck')])
+    for cat in categories:
+        inputs[cat][value] = sum([map.hist for map in template if
+            map.name.endswith(cat)])
 
-cscd_array = []
-trck_array = []
+arrays = {}
+for cat in categories:
+    arrays[cat] = []
+
 for x in x_values:
-    cscd_array.append(cscd[x]/cscd[nominal])
-    trck_array.append(trck[x]/trck[nominal])
+    for cat in categories:
+        arrays[cat].append(inputs[cat][x]/inputs[cat][nominal])
 
-cscd_array = np.array(cscd_array).transpose(1,2,0)
-trck_array = np.array(trck_array).transpose(1,2,0)
+for cat in categories:
+    arrays[cat] = np.array(arrays[cat]).transpose(1,2,0)
 
-cscd_slopes = np.ones_like(cscd[nominal])
-nx, ny = cscd_slopes.shape
+nx, ny = inputs[categories[0]][nominal].shape
 bins_x = np.arange(nx)
 bins_y = np.arange(ny)
 
@@ -93,42 +113,50 @@ grid_y = np.ravel(grid_y)
 # shift to get deltas
 x_values -= nominal
 
-# fix point poly definition
-if force_through_nominal:
-    fit_fun = lambda x, *p: polyval(x, [1.] + list(p))
-else:
-    fit_fun = lambda x, *p: polyval(x, list(p))
+# array to store params
+outputs = {}
+for cat in categories:
+    outputs[cat] = np.ones((nx, ny, degree))
 
-for i, j in np.ndindex(cscd_slopes.shape):
-    y_values = cscd_array[i,j,:]
-    popt, pcov = curve_fit(fit_fun, x_values,
-            y_values, p0=np.ones(degree))
-    cscd_slopes[i,j] = popt[0]
-    if args.plot:
-        fig_num = i + nx * j
-        if fig_num == 0:
-            fig = plt.figure(num=1, figsize=( 4*nx, 4*ny))
-        subplot_idx = nx*(ny-1-j)+ i + 1
-        plt.subplot(ny, nx, subplot_idx)
-        plt.scatter(x_values, y_values, color='blue')
-        f_values = fit_fun(x_values, *popt)
-        fun_plot, = plt.plot(x_values, f_values,'k-')
-        plt.ylim(np.min(cscd_array)*0.9, np.max(cscd_array)*1.1)
-        if i > 0:
-            plt.setp(plt.gca().get_yticklabels(), visible=False)
-        if j > 0:
-            plt.setp(plt.gca().get_xticklabels(), visible=False)
+    for i, j in np.ndindex((nx,ny)):
+    #for i, j in np.ndindex(inputs[cat][nominal].shape):
+        y_values = arrays[cat][i,j,:]
+        popt, pcov = curve_fit(fit_fun, x_values,
+                y_values, p0=np.ones(degree))
+        for k, p in enumerate(popt):
+            outputs[cat][i,j,k] = p
+        # plot
+        if args.plot:
+            fig_num = i + nx * j
+            if fig_num == 0:
+                fig = plt.figure(num=1, figsize=( 4*nx, 4*ny))
+            subplot_idx = nx*(ny-1-j)+ i + 1
+            plt.subplot(ny, nx, subplot_idx)
+            plt.scatter(x_values, y_values, color=plt_colors[cat])
+            # nominal point
+            plt.scatter([0.0], [1.0], color='k')
+            f_values = fit_fun(x_values, *popt)
+            fun_plot, = plt.plot(x_values, f_values,
+                    color=plt_colors[cat])
+            plt.ylim(np.min(arrays[cat])*0.9, np.max(arrays[cat])*1.1)
+            if i > 0:
+                plt.setp(plt.gca().get_yticklabels(), visible=False)
+            if j > 0:
+                plt.setp(plt.gca().get_xticklabels(), visible=False)
 
 if args.plot:
     fig.subplots_adjust(hspace=0)
     fig.subplots_adjust(wspace=0)
     plt.show()
-    plt.savefig('cscd.pdf')
+    plt.savefig('%s_sysfits.pdf'%pname)
+    plt.clf()
 
-
+outputs['pname'] = pname
+outputs['nominal'] = nominal
+outputs['function'] = function
+to_file(outputs, './%s_sysfits.json'%pname)
 
 #spline = interpolate.SmoothBivariateSpline(grid_x, grid_y,
 #        np.ravel(cscd_slopes), kx=2, ky=2)
 #smooth = spline(bins_x, bins_y)
-
 #plt.imshow(smooth,interpolation='nearest')
