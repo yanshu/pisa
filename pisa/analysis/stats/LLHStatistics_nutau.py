e2bce2d1e18d788798f6b8198cc692e7d4140169
yanshu <huangfeifei91@gmail.com>
2016-07-15 01:20:59 -0400

    added get_chi2()

--- pisa/analysis/stats/LLHStatistics_nutau.py
+++ pisa/analysis/stats/LLHStatistics_nutau.py
@@ -3,8 +3,10 @@
 # calculations on a set of templates. Operates on templates, which are
 # numpy arrays. No input/output operations or reading from customized
 import numpy as np
+import scipy
 from scipy.special import gammaln
 from pisa.analysis.stats.barlow_llh import likelihoods
+from pisa.utils.params import get_param_priors
 
 def log_poisson(k,l):
     return k*np.log(l) -l - gammaln(k+1)
@@ -20,8 +22,30 @@ def conv_poisson(k,l,s,nsigma=3,steps=100.):
     # avoid zero values for lambda
     idx = np.argmax(f_x>0)
     f_y = log_poisson(k,f_x[idx:])
+    if np.isnan(f_y).any():
+        print "idx = ", idx
+        print "s = ", s
+        print "l = ", l
+        print "f_x = ", f_x
+        print "f_y = ", f_y
+    f_y = np.nan_to_num(f_y)
     conv = np.exp(conv_y[idx:] + f_y)
     return conv.sum()*(conv_x[1]-conv_x[0])
+
+def get_chi2(data, mc, data_sumw2, mc_sumw2, params, priors):
+    """
+    Computes the chi2 of the (pseudo) data and the best fit
+    template, where each input is expected to be a 2d numpy array
+    """
+    chi2 = np.sum((data-mc)**2/(data_sumw2 + mc_sumw2))
+    chi2 += sum([prior.chi2(val) for (val, prior) in zip(params, priors)])
+    no_uniform_prior = 0
+    for prior in priors:
+        if prior.kind == 'uniform':
+            no_uniform_prior += 1
+    dof = len(data) - no_uniform_prior 
+    chi2_p = scipy.stats.chi2.sf(chi2, df=dof)
+    return chi2, chi2_p, dof
 
 def get_binwise_llh(pseudo_data, template):
     """
