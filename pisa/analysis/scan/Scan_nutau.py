bb96ce2a7aac22178a15b4fb38f00c081b23538d
yanshu <huangfeifei91@gmail.com>
2016-09-07 20:28:50 -0400

    make scan work

--- pisa/analysis/scan/Scan_nutau.py
+++ pisa/analysis/scan/Scan_nutau.py
@@ -14,12 +14,13 @@ from pisa.utils.log import logging, physics, profile
 from pisa.utils.params import get_values, select_hierarchy, get_fixed_params, get_free_params, get_param_values, get_param_scales, get_param_bounds, get_param_priors
 from pisa.analysis.stats.Maps import flatten_map
 from pisa.analysis.stats.Maps_nutau import get_true_template
-from pisa.analysis.stats.LLHStatistics_nutau import get_binwise_llh
+from pisa.analysis.stats.LLHStatistics_nutau import get_binwise_llh, get_chi2
 from pisa.analysis.scan.Scan import calc_steps
 
 
-def find_max_grid(fmap, template_maker, params, grid_settings, save_steps=True,
-                  normal_hierarchy=True):
+def find_max_grid(num_data_events, fmap, template_maker, params,
+                  grid_settings, save_steps=True, normal_hierarchy=True,
+                  check_octant=False, use_chi2=False, use_rnd_init=False):
     '''
     Finds the template (and free systematic params) that maximize
     likelihood that the data came from the chosen template of true
@@ -55,10 +56,14 @@ def find_max_grid(fmap, template_maker, params, grid_settings, save_steps=True,
 
     #Build a list from all parameters that holds a list of (name, step) tuples
     steplist = [ [(name,step) for step in param['steps']] for name, param in sorted(free_params.items())]
+    print "steplist = ", steplist
 
     #Prepare to store all the steps
     steps = {key:[] for key in free_params.keys()}
     steps['llh'] = []
+    steps['chi2'] = []
+    steps['chi2_p'] = []
+    steps['dof'] = []
 
     #Iterate over the cartesian product
     for pos in product(*steplist):
@@ -74,12 +79,30 @@ def find_max_grid(fmap, template_maker, params, grid_settings, save_steps=True,
 
         # Now get true template
         profile.info('start template calculation')
-        true_fmap = get_true_template(template_params, template_maker)
+        # normal
+        #true_fmap = get_true_template(template_params,template_maker, num_data_events) 
+        # smeared
+        true_fmap, true_fmap_sumw2 = get_true_template(template_params,template_maker,num_data_events,error=True) 
+        # barlow
+        #map_nu, map_mu, sumw2_nu, sumw2_mu = get_true_template(template_params, template_maker, num_data_events, error=True, both=True)
 
         profile.info('stop template calculation')
 
         # and calculate the likelihood
-        llh = -get_binwise_llh(fmap, true_fmap, template_params)
+        # normal
+        llh = -get_binwise_llh(fmap,true_fmap)
+        # smeared
+        #llh = -get_binwise_smeared_llh(fmap, true_fmap, sumw2_map)
+        # barlow
+        #llh = get_barlow_llh(fmap, map_nu, sumw2_nu, map_mu, sumw2_mu)
+
+        # get chi2
+        fmap_sumw2 = true_fmap
+        #free_params_vals = get_param_values(free_params)
+        free_params_vals = [v for k,v in sorted(pos)]
+        print "free_params_vals = ", free_params_vals
+        chi2, chi2_p, dof = get_chi2(fmap, true_fmap, fmap_sumw2, true_fmap_sumw2, free_params_vals, priors)
+
 
         # get sorted vals to match with priors
         vals = [ v for k,v in sorted(pos) ]
@@ -87,20 +110,41 @@ def find_max_grid(fmap, template_maker, params, grid_settings, save_steps=True,
 
         # Save all values to steps and report
         steps['llh'].append(llh)
+        steps['chi2'].append(chi2)
+        steps['chi2_p'].append(chi2_p)
         physics.debug("LLH is %.2f at: "%llh)
         for key, val in pos:
             steps[key].append(val)
             physics.debug(" %20s = %6.4f" %(key, val))
+    steps['dof']= [dof]
 
     #Find best fit value
     maxllh = min(steps['llh'])
-    maxpos = steps['llh'].index(maxllh)
+    maxpos_llh = steps['llh'].index(maxllh)
+    minchi2 = min(steps['chi2'])
+    minpos_chi2 = steps['chi2'].index(minchi2)
+    maxchi2p = max(steps['chi2_p'])
+    maxpos_chi2p = steps['chi2_p'].index(maxchi2p)
 
+    print "steps.items() = ", steps.items()
     #Report best fit
-    physics.info('Found best LLH = %.2f in %d calls at:'
-                 %(maxllh,len(steps['llh'])))
-    for name, vals in steps.items():
-        physics.info('  %20s = %6.4f'%(name,vals[maxpos]))
+    if use_chi2:
+        physics.info('Found best chi2 = %.2f in %d calls at:' %(minchi2,len(steps['chi2'])))
+        for name, vals in steps.items():
+            if name == 'dof':
+                continue
+            physics.info('  %20s = %6.4f'%(name,vals[minpos_chi2]))
+        physics.info('Found best chi2_p = %.2f in %d calls at:' %(maxchi2p,len(steps['chi2_p'])))
+        for name, vals in steps.items():
+            if name == 'dof':
+                continue
+            physics.info('  %20s = %6.4f'%(name,vals[maxpos_chi2p]))
+    else:
+        physics.info('Found best LLH = %.2f in %d calls at:' %(maxllh,len(steps['llh'])))
+        for name, vals in steps.items():
+            if name == 'dof':
+                continue
+            physics.info('  %20s = %6.4f'%(name,vals[maxpos_llh]))
 
         #only save this maximum if asked for
         if not save_steps:
