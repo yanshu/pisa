9e90718e87ce17f9f4b20ae2eca641658b56abcb
yanshu <huangfeifei91@gmail.com>
2016-09-13 17:53:12 -0400

    set a parameter to a certain value

--- nutau/plot.py
+++ nutau/plot.py
@@ -5,6 +5,7 @@ from pisa import ureg, Q_
 from pisa.core.distribution_maker import DistributionMaker
 from pisa.utils.log import set_verbosity
 from pisa.utils.plotter import plotter
+from pisa.utils.jsons import from_json, to_json
 
 parser = ArgumentParser()
 parser.add_argument('-t', '--template-settings',
@@ -14,6 +15,8 @@ parser.add_argument('-t', '--template-settings',
 parser.add_argument('-d', '--data-settings', type=str,
 		    metavar='configfile', default=None,
 		    help='settings for the generation of "data"')
+parser.add_argument('-sp', '--set-param', type=str, default='',
+                    help='Set a param to a certain value.')
 parser.add_argument('-v', action='count', default=None,
                     help='set verbosity level')
 parser.add_argument('--livetime', type=float, default=None)
@@ -30,6 +33,19 @@ else:
     data = None
     stamp=r'$\nu_\tau$ appearance'+'\nMC'
 
+if not args.set_param == '':
+    p_name,value = args.set_param.split("=")
+    print "p_name,value= ", p_name, " ", value
+    value = parse_quantity(value)
+    value = value.n * value.units
+    test = template_maker.params[p_name]
+    test.value = value
+    template_maker.update_params(test)
+    if args.data_settings is not None and p_name in data_maker.params.names:
+        test = data_maker.params[p_name]
+        test.value = value
+        data_maker.update_params(test)
+
 my_plotter = plotter(stamp=stamp, outdir='.', fmt='pdf', log=False, annotate=True, symmetric=False, ratio=True)
 
 template_maker = DistributionMaker(args.template_settings)
@@ -40,6 +56,8 @@ if args.livetime is not None:
 
 template_nominal = template_maker.get_outputs()
 for map in template_nominal: map.tex = 'MC'
+
+print "template_nominal = ", template_nominal['cscd'].hist
 
 my_plotter.plot_1d_array(template_nominal, 'reco_coszen', fname='p_coszen')
 my_plotter.plot_1d_array(template_nominal, 'reco_energy', fname='p_energy')
