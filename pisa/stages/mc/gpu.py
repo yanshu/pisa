6241b788cd5e6271b0ddf59ce86d6ff4e7ec7141
Philipp Eller <peller@phys.ethz.ch>
2016-09-29 09:11:21 -0400

    the documentation saga continues...

--- pisa/stages/mc/gpu.py
+++ pisa/stages/mc/gpu.py
@@ -41,6 +41,7 @@ class gpu(Stage):
             deltam31 : quantity (mass^2)
             deltacp : quantity (angle)
             no_nc_osc : bool
+                don't oscillate NC events, but rather assign probabilities 1 for nue->nue and numu->numu, and 0 for nutau->nutau
             nu_nubar_ratio : quantity (dimensionless)
             nue_numu_ratio : quantity (dimensionless)
             livetime : quantity (time)
@@ -59,6 +60,38 @@ class gpu(Stage):
             reco_e_scale_raw : quantity (dimensionless)
             reco_cz_res_raw :quantity (dimensionless)
             bdt_cut : quantity (dimensionless)
+
+    Notes
+    -----
+
+    This stage takes as inputs hdf5 events files that already iclude the nominal flux weights and genie systematic coefficients. The files are expected to contain
+        true_energy : true energy of the event in GeV
+        true_coszen : true coszen of the event
+        reco_energy : reco energy of the event in GeV
+        reco_coszen : reco coszen of the event
+        neutrino_nue_flux : flux weight for nu_e in case of neutrino, or anit-nu_e in case of anti-neutrino event
+        neutrino_numu_flux : flux weight for nu_mu in case of neutrino, or anit-nu_mu in case of anti-neutrino event
+        neutrino_oppo_nue_flux:flux weight for anti-nu_e in case of neutrino, or nu_e in case of anti-neutrino event
+        neutrino_oppo_numu_flux :flux weight for anti-nu_mu in case of neutrino, or nu_mu in case of anti-neutrino event
+        weighted_aeff : effective are weight for event
+        pid : pid value
+        dunkman_l5 : BDT value
+        linear_fit_maccqe : Genie CC quasi elastic linear coefficient
+        quad_fit_maccqe : Genie CC quasi elastic quadratic coefficient
+        linear_fit_maccres : Genie CC resonance linear coefficient
+        quad_fit_maccres : Genie CC resonance quadratic coefficient
+
+
+    the dictionary self.events_dict is the central object here:
+    it contains two dictionaries for every flavour:
+        * host : all event arrays on CPU
+        * device : pointers to the same arrays, but on GPU
+    and some additional keys like:
+        * n_evts : number of events
+        * hist : retreived histogram
+        * ...
+
+    All floats (arrays) on GPU are of type FTYPE, currently double precision
         
     '''
 
@@ -126,7 +159,8 @@ class gpu(Stage):
         )
 
     def _compute_nominal_outputs(self):
-
+        
+        # these are for storing hashes for caching that is done inside the stage
         self.osc_hash = None
         self.weight_hash = None
 
@@ -141,6 +175,7 @@ class gpu(Stage):
         prop_height = self.params.prop_height.value.m_as('km')
         detector_depth = self.params.detector_depth.value.m_as('km')
 
+        # Prob3 GPU oscialltions
         self.osc = Prob3GPU(detector_depth,
                             earth_model,
                             prop_height,
@@ -148,6 +183,7 @@ class gpu(Stage):
                             YeO,
                             YeM)
 
+        # Weight calculator
         self.weight = GPUweight()
 
         self.e_dim_num = self.output_binning.names.index('reco_energy')
@@ -162,6 +198,7 @@ class gpu(Stage):
                 bin_edges = self.output_binning[name].bin_edges.magnitude.astype(FTYPE)
             self.bin_edges.append(bin_edges)
             
+        # GPU histogramer
         self.histogrammer = GPUhist(*self.bin_edges)
 
         # load events
@@ -174,20 +211,54 @@ class gpu(Stage):
         bdt_cut = self.params.bdt_cut.value.m_as('dimensionless')
 
         # Load and copy events
-        variables = ['true_energy', 'true_coszen', 'reco_energy', 'reco_coszen',
-                    'neutrino_nue_flux', 'neutrino_numu_flux', 'neutrino_oppo_nue_flux',
-                    'neutrino_oppo_numu_flux', 'weighted_aeff', 'pid', 'dunkman_L5',
-                    'linear_fit_MaCCQE', 'quad_fit_MaCCQE',
-                    'linear_fit_MaCCRES', 'quad_fit_MaCCRES',
+        variables = ['true_energy',
+                        'true_coszen',
+                        'reco_energy',
+                        'reco_coszen',
+                        'neutrino_nue_flux',
+                        'neutrino_numu_flux',
+                        'neutrino_oppo_nue_flux',
+                        'neutrino_oppo_numu_flux',
+                        'weighted_aeff',
+                        'pid',
+                        'dunkman_L5',
+                        'linear_fit_MaCCQE',
+                        'quad_fit_MaCCQE',
+                        'linear_fit_MaCCRES',
+                        'quad_fit_MaCCRES',
                     ]
-        # to allocate empty arrays on GPU
-        empty = ['prob_e', 'prob_mu', 'weight_trck', 'weight_cscd', 'scaled_nue_flux', 'scaled_numu_flux', 'scaled_nue_flux_shape', 'scaled_numu_flux_shape']
+        # allocate empty arrays (filled with 1s) on GPU
+        empty = ['prob_e',
+                    'prob_mu',
+                    'weight_trck',
+                    'weight_cscd',
+                    'scaled_nue_flux',
+                    'scaled_numu_flux',
+                    'scaled_nue_flux_shape',
+                    'scaled_numu_flux_shape'
+                ]
+
         if self.error_method in ['sumw2', 'fixed_sumw2']:
-            empty += ['sumw2_trck', 'sumw2_cscd']
+            empty += ['sumw2_trck',
+                        'sumw2_cscd'
+                    ]
 
         # list of flav_ints to use and corresponding number used in several parts of the code
-        self.flavs = ['nue_cc', 'numu_cc', 'nutau_cc', 'nue_nc', 'numu_nc', 'nutau_nc',
-                'nuebar_cc', 'numubar_cc', 'nutaubar_cc', 'nuebar_nc', 'numubar_nc', 'nutaubar_nc']
+        self.flavs = ['nue_cc',
+                        'numu_cc',
+                        'nutau_cc',
+                        'nue_nc',
+                        'numu_nc',
+                        'nutau_nc',
+                        'nuebar_cc',
+                        'numubar_cc',
+                        'nutaubar_cc',
+                        'nuebar_nc',
+                        'numubar_nc',
+                        'nutaubar_nc'
+                    ]
+
+        # corrwsponding numbers for the flavours defined above, needed bi Prob3
         kFlavs = [0, 1, 2] * 4
         kNuBars = [1] *6 + [-1] * 6
 
@@ -228,12 +299,18 @@ class gpu(Stage):
             #cut[9::10] = 1
             #self.events_dict[flav]['host']['weighted_aeff']*=cut
             for var in empty:
-                if self.params.no_nc_osc and ( (flav in ['nue_nc', 'nuebar_nc'] and var == 'prob_e') or (flav in ['numu_nc', 'numubar_nc'] and var == 'prob_mu') ):
+                if self.params.no_nc_osc and ( (flav in ['nue_nc', 'nuebar_nc'] and var == 'prob_e') or \
+                        (flav in ['numu_nc', 'numubar_nc'] and var == 'prob_mu') ):
+                    # in case of not oscillating NC events, we can set the probabilities of nue->nue and numu->numu at 1, and nutau->nutau at 0
                     self.events_dict[flav]['host'][var] = np.ones(self.events_dict[flav]['n_evts'], dtype=FTYPE)
                 else:
                     self.events_dict[flav]['host'][var] = np.zeros(self.events_dict[flav]['n_evts'], dtype=FTYPE)
-            # calulate layers
-            self.events_dict[flav]['host']['numLayers'], self.events_dict[flav]['host']['densityInLayer'], self.events_dict[flav]['host']['distanceInLayer'] = self.osc.calc_Layers(self.events_dict[flav]['host']['true_coszen'])
+            # calulate layers (every particle crosses a number of layers in the earth with different densities, and for a given length
+            # these depend only on the earth model (PREM) and the true coszen of an event. Therefore we can calculate these for once and are done
+            self.events_dict[flav]['host']['numLayers'], \
+                self.events_dict[flav]['host']['densityInLayer'], \
+                self.events_dict[flav]['host']['distanceInLayer'] = \
+                self.osc.calc_Layers(self.events_dict[flav]['host']['true_coszen'])
         end_t = time.time()
         logging.debug('layers done in %.4f ms'%((end_t - start_t) * 1000))
 
@@ -261,10 +338,15 @@ class gpu(Stage):
 
             # apply coszen reco sys
             f = self.params.reco_cz_res_raw.value.m_as('dimensionless')
-            self.events_dict[flav]['host']['reco_coszen'] = ((1.-f) * self.events_dict[flav]['host']['true_coszen'] + f * self.events_dict[flav]['host']['reco_coszen']).astype(FTYPE)
+            self.events_dict[flav]['host']['reco_coszen'] = \
+                ((1.-f) * self.events_dict[flav]['host']['true_coszen'] + \
+                f * self.events_dict[flav]['host']['reco_coszen']).astype(FTYPE)
+            # make sure everything is within -1 <= coszen <= 1, otherwise reflect
             while np.any(self.events_dict[flav]['host']['reco_coszen']<-1) or np.any(self.events_dict[flav]['host']['reco_coszen']>1):
-                self.events_dict[flav]['host']['reco_coszen'][self.events_dict[flav]['host']['reco_coszen']>1] = 2-self.events_dict[flav]['host']['reco_coszen'][self.events_dict[flav]['host']['reco_coszen']>1]
-                self.events_dict[flav]['host']['reco_coszen'][self.events_dict[flav]['host']['reco_coszen']<-1] = -2-self.events_dict[flav]['host']['reco_coszen'][self.events_dict[flav]['host']['reco_coszen']<-1]
+                self.events_dict[flav]['host']['reco_coszen'][self.events_dict[flav]['host']['reco_coszen']>1] = \
+                    2-self.events_dict[flav]['host']['reco_coszen'][self.events_dict[flav]['host']['reco_coszen']>1]
+                self.events_dict[flav]['host']['reco_coszen'][self.events_dict[flav]['host']['reco_coszen']<-1] = \
+                    -2-self.events_dict[flav]['host']['reco_coszen'][self.events_dict[flav]['host']['reco_coszen']<-1]
 
             self.update_device_arrays(flav, 'reco_energy')
             self.update_device_arrays(flav, 'reco_coszen')
@@ -350,7 +432,7 @@ class gpu(Stage):
                                     Barr_uphor_ratio=Barr_uphor_ratio, Barr_nu_nubar_ratio=Barr_nu_nubar_ratio,
                                     **self.events_dict[flav]['device'])
 
-                # calculate global scales
+                # calculate global scales for flux normalizations
                 #nue_flux_norm_n = self.sum_array(self.events_dict[flav]['device']['scaled_nue_flux'], self.events_dict[flav]['n_evts'])
                 #nue_flux_norm_d = self.sum_array(self.events_dict[flav]['device']['scaled_nue_flux_shape'], self.events_dict[flav]['n_evts'])
                 #nue_flux_norm = nue_flux_norm_n / nue_flux_norm_d
@@ -407,8 +489,7 @@ class gpu(Stage):
         self.osc_hash = osc_hash
         self.weight_hash = weight_hash
 
-        maps = []
-        # apply scales, add up all cscds and tracks, and pack them up in final PISA MapSet
+        # apply scales, add up all cscds and tracks
         for i,flav in enumerate(self.flavs):
             #if 'nutau' in flav:
             if flav in ['nutau_cc','nutaubar_cc']:
@@ -418,7 +499,7 @@ class gpu(Stage):
             else:
                 f = 1.0
 
-            # add up
+            # add up all cscd and trck events
             if i == 0:
                 hist_cscd = np.copy(self.events_dict[flav]['hist_cscd']) * f
                 hist_trck = np.copy(self.events_dict[flav]['hist_trck']) * f
@@ -432,6 +513,8 @@ class gpu(Stage):
                     sumw2_cscd += self.events_dict[flav]['sumw2_cscd'] * f * f
                     sumw2_trck += self.events_dict[flav]['sumw2_trck'] * f * f
 
+        maps = []
+        # pack everything in final PISA MapSet
         if self.error_method == 'sumw2':
             maps.append(Map(name='cscd', hist=hist_cscd, error_hist=np.sqrt(sumw2_cscd), binning=self.output_binning))
             maps.append(Map(name='trck', hist=hist_trck, error_hist=np.sqrt(sumw2_trck), binning=self.output_binning))
