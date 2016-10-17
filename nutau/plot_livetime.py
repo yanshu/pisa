6d3d8f8463119c06c69c35fefeb8559c0148be70
Philipp Eller <peller@phys.ethz.ch>
2016-10-17 14:44:46 -0400

    plots

--- nutau/plot_livetime.py
+++ nutau/plot_livetime.py
@@ -26,15 +26,21 @@ def plot(name,hypos,asimovs,x_var='nutau_cc_norm',dir='.'):
     fig = plt.figure()
     plt.grid()
     fig.patch.set_facecolor('none')
-    ax = fig.add_subplot(111)
+    ax = plt.subplot2grid((1,8), (0,0), colspan=7)
+    plt.grid()
+    fig.subplots_adjust(wspace=0)
+    plt.subplots_adjust(right=0.97)
+    #ax = fig.add_subplot(111)
     #a_text = AnchoredText(r'DeepCore $\nu_\tau$ appearance', loc=2, frameon=False)
-    a_text = AnchoredText(r'PINGU v47 $\nu_\tau$ appearance', loc=2, frameon=False)
+    a_text = AnchoredText('Gen2 Phase 1 Preliminary\n'+r'$\nu_\tau$ Appearance', loc=2, frameon=False)
     ax.add_artist(a_text)
     sigmas = []
-    years = []
     minorLocator = MultipleLocator(0.1)
     for i,asimov in enumerate(asimovs): 
+        print i
+        years = []
         years.append(sorted(asimov.keys()))
+        years = np.array(years)
         times = []
         best = []
         best_ms = []
@@ -59,22 +65,26 @@ def plot(name,hypos,asimovs,x_var='nutau_cc_norm',dir='.'):
             best_ps.append(np.interp(1,data[best_idx:],hypos[best_idx:]))
             #best_p2s.append(np.interp(2.71,data[best_idx:],hypos[best_idx:]))
             best_p2s.append(np.interp(4.,data[best_idx:],hypos[best_idx:]))
+        years = years/12.
+        for j,year in enumerate(years[-1]):
+            print '%.1f years:\t %.3f, %.3f, %.3f, %.3f, %.3f'%(year, best_m2s[j], best_ms[j], best[j], best_ps[j], best_p2s[j])
         if i == 0:
             #ax.fill_between(years[-1],best_m2s,best_p2s,facecolor='g', linewidth=0, alpha=0.15, label='90% range improved sys')
             #ax.fill_between(years[-1],best_ms,best_ps,facecolor='g', linewidth=0, alpha=0.3, label='68% range improved sys')
             ax.fill_between(years[-1],best_m2s,best_p2s,facecolor='g', linewidth=0, alpha=0.15, label='95% range')
             ax.fill_between(years[-1],best_ms,best_ps,facecolor='g', linewidth=0, alpha=0.3, label='68% range')
-            ax.text(max(years[-1])*1.01,0.98*best_ms[-1],' %.2f'%best_ms[-1],fontsize=10,color='g')
-            ax.text(max(years[-1])*1.01,0.98*best_ps[-1],' %.2f'%best_ps[-1],fontsize=10,color='g')
-            ax.text(max(years[-1])*1.01,0.98*best_m2s[-1],'(%.2f)'%best_m2s[-1],fontsize=10,color='g')
-            ax.text(max(years[-1])*1.01,0.98*best_p2s[-1],'(%.2f)'%best_p2s[-1],fontsize=10,color='g')
+            
+            #ax.text(max(years[-1])*1.01,0.98*best_ms[-1],' %.2f'%best_ms[-1],fontsize=10,color='g')
+            #ax.text(max(years[-1])*1.01,0.98*best_ps[-1],' %.2f'%best_ps[-1],fontsize=10,color='g')
+            #ax.text(max(years[-1])*1.01,0.98*best_m2s[-1],'(%.2f)'%best_m2s[-1],fontsize=10,color='g')
+            #ax.text(max(years[-1])*1.01,0.98*best_p2s[-1],'(%.2f)'%best_p2s[-1],fontsize=10,color='g')
         else:
             ax.plot(years[-1],best_m2s, color='k', linewidth=1, linestyle=':', label='95% range baseline')
             ax.plot(years[-1],best_p2s, color='k', linewidth=1, linestyle=':')
             ax.plot(years[-1],best_ms,  color='k', linewidth=1, label='68% range baseline')
             ax.plot(years[-1],best_ps,  color='k', linewidth=1)
-        #ax.set_xlabel('livetime (years)')
-        ax.set_xlabel('livetime (months)')
+        ax.set_xlabel('livetime (years)')
+        #ax.set_xlabel('livetime (months)')
         ax.set_ylabel(r'$\nu_\tau$ normalization')
         ax.set_ylim([0,2])
         #ax.set_yticks(np.linspace(0,2,21))
@@ -84,6 +94,23 @@ def plot(name,hypos,asimovs,x_var='nutau_cc_norm',dir='.'):
         ax.plot(years[-1],best, color='g', linewidth=1)
     
     ax.legend(loc='upper right',ncol=1, frameon=False,numpoints=1,fontsize=10)
+
+    ax2 = plt.subplot2grid((1,8), (0,7),sharey=ax)
+    ax2.errorbar(np.array([-1.]),np.array([1.]),yerr=np.array([[1.0-best_ms[-1]],[best_ps[-1]-1.0]]),fmt='.',color='forestgreen')
+    ax2.text(-1.0,best_ms[-1],'this analysis (%iy exp.) 68%% '%(years[0][-1]),fontsize=10,color='g',rotation='vertical',ha='center',va='top')
+    #ax2.errorbar(np.array([0.]),np.array([1.]),yerr=np.array([[0.20],[0.21]]),fmt='.',color='forestgreen')
+    ax2.errorbar(np.array([0.]),np.array([1.]),yerr=np.array([[0.15],[0.17]]),fmt='.',color='forestgreen')
+    #e = ax2.errorbar(np.array([0.]),np.array([1.]),yerr=np.array([[0.31],[0.36]]),fmt='.',color='forestgreen')
+    #e[-1][0].set_linestyle(':')
+    #ax2.text(0.0,1.0-0.31,'DeepCore (10y exp.) 68%, 95% ',fontsize=10,color='g',rotation='vertical',ha='center',va='top')
+    ax2.text(0.0,1.0-0.15,'DeepCore (10y exp.) 68% ',fontsize=10,color='g',rotation='vertical',ha='center',va='top')
+    ax2.errorbar(np.array([1.]),np.array([1.47]),yerr=np.array([[0.32],[0.32]]),fmt='.',color='navy')
+    ax2.text(1.0,1.47-0.32,'SuperK 68% ',fontsize=10,color='navy',rotation='vertical',ha='center',va='top')
+    ax2.errorbar(np.array([2.]),np.array([1.8]),yerr=np.array([[1.1],[1.8]]),fmt='.',color='navy')
+    ax2.text(2.0,1.8-1.1,'Opera 90% ',fontsize=10,color='navy',rotation='vertical',ha='center',va='top')
+    ax2.set_xlim(-2,3)
+    ax2.set_ylim(0,2)
+    ax2.set_axis_off()
 
     plt.show()
     plt.savefig('%s/nutaunorm.png'%(dir), facecolor=fig.get_facecolor(), edgecolor='none')
@@ -118,16 +145,20 @@ if __name__ == '__main__':
 
     parser = ArgumentParser()
     parser.add_argument('-d','--dir',metavar='dir',help='directory containg output json files', default='.') 
-    parser.add_argument('-d1','--dir1',metavar='dir',help='directory containg output json files', default='.') 
+    parser.add_argument('-d1','--dir1',metavar='dir',help='directory containg output json files', default=None) 
     parser.add_argument('-x','--x-var',help='variable to plot against', default='nutau_cc_norm') 
     parser.add_argument('--dist',action='store_true') 
     parser.add_argument('--asimov',action='store_true') 
     args = parser.parse_args()
 
     #asimovs = [{},{}]
-    asimovs = [{}]
+    asimovs = []
+    dirs = [args.dir]
+    if args.dir1 is not None:
+        dirs.append(args.dir1)
 
-    for i,dir in enumerate([args.dir]):
+    for i,dir in enumerate(dirs):
+        asimovs.append({})
         # get llh denominators for q for each seed
         for filename in os.listdir(dir):
             if filename.endswith('.json'):
