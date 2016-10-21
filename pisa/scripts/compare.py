92fbc44eb828028cb6f6686bff1359ac0785fb3c
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-10-21 00:51:02 -0400

    compare (almost) anything

--- pisa/scripts/compare.py
+++ pisa/scripts/compare.py
@@ -5,13 +5,15 @@
 Compares reco vbwkde vs. hist.
 """
 
+
 from argparse import ArgumentParser
 from collections import Iterable
 
 import numpy as np
 from uncertainties import unumpy as unp
 
-from pisa.core.pipeline import Pipeline
+from pisa.core.distribution_maker import DistributionMaker
+from pisa.core.map import Map, MapSet
 from pisa.utils.fileio import mkdir
 from pisa.utils.log import logging, set_verbosity
 from pisa.utils.tests import plot_cmp
@@ -87,37 +89,129 @@ if __name__ == '__main__':
     if args.png:
         plot_formats.append('png')
 
+    mkdir(args.outdir)
+
+    # Get the reference distribution(s) into the form of a test MapSet
     ref = None
+    ref_source = None
     try:
-        ref_pipeline = DistributionMaker(config=args.ref)
+        ref_dmaker = DistributionMaker(config=args.ref)
     except:
         pass
-    try
-        test_pipeline = Pipeline(config=args.test)
+    else:
+        ref_source = 'DistributionMaker'
+        if args.ref_param_selections is not None:
+            ref_dmaker.select_params(args.ref_param_selections)
+        ref = ref_dmaker.get_outputs()
 
-    if args.outdir is not None:
-        mkdir(args.outdir)
+    if ref is None:
+        assert args.ref_param_selections is None
+        try:
+            ref = MapSet.from_file(args.ref)
+        except:
+            pass
+        else:
+            ref_source = 'MapSet'
 
-    ref_maps = ref_pipeline.get_outputs()
-    test_maps = test_pipeline.get_outputs()
+    if ref is None:
+        try:
+            ref = Map.from_file(args.ref)
+        except:
+            pass
+        else:
+            ref_source = 'Map'
+            ref = MapSet([ref])
 
-    if test_maps.names != ref_maps.names:
+    if ref is None:
         raise ValueError(
-            'Test map names %s do not match ref map names %s.'
-            %(test_maps.names, ref_maps.names)
+            'Could not instantiate a DistributionMaker, Map, or MapSet from'
+            ' ref valu(s) %s' % args.ref
+        )
+
+    # Get the test distribution(s) into the form of a test MapSet
+    test = None
+    test_source = None
+    try:
+        test_dmaker = DistributionMaker(config=args.test)
+    except:
+        pass
+    else:
+        test_source = 'DistributionMaker'
+        if args.test_param_selections is not None:
+            test_dmaker.select_params(args.test_param_selections)
+        test = test_dmaker.get_outputs()
+
+    if test is None:
+        assert args.test_param_selections is None
+        try:
+            test = MapSet.from_file(args.test)
+        except:
+            pass
+        else:
+            test_source = 'MapSet'
+
+    if test is None:
+        try:
+            test = Map.from_file(args.test)
+        except:
+            pass
+        else:
+            test_source = 'Map'
+            test = MapSet([test])
+
+    if ref is None:
+        raise ValueError(
+            'Could not instantiate a DistributionMaker, Map, or MapSet from'
+            ' ref valu(s) %s' % args.ref
+        )
+    if test is None:
+        raise ValueError(
+            'Could not instantiate a DistributionMaker, Map, or MapSet from'
+            ' test valu(s) %s' % args.test
         )
 
     if args.combine is not None:
         for cmb_str in args.combine:
-            ref_maps = ref_maps.combine_wildcard(comb_str)
-            test_maps = test_maps.combine_wildcard(comb_str)
+            ref = ref.combine_wildcard(comb_str)
+            test = test.combine_wildcard(comb_str)
 
-    my_plotter = Plotter(stamp='', outdir=args.outdir, fmt='pdf', log=False, annotate=False, symmetric=False, ratio=True)
-    for map in ref_maps:
-        print '%s:\t%.2f'%(map.name, np.sum(unp.nominal_values(map.hist)))
-    my_plotter.plot_2d_array(ref_maps, split_axis='pid', fname='%s_nominal'%args.ref_label)
-    my_plotter.plot_2d_array(test_maps, split_axis='pid', fname='%s_nominal'%args.test_label)
-    my_plotter.label = '%s/%s - 1'%(args.test_label, args.ref_label)
-    my_plotter.plot_2d_array(test_maps/ref_maps-1., split_axis='pid', fname='ratio', cmap='RdBu', vmin=-2,vmax=2)
-    my_plotter.label = '%s - %s'%(args.ref_label, args.test_label)
-    my_plotter.plot_2d_array(ref_maps - test_maps, split_axis='pid', fname='abs_diff', cmap='RdBu', vmin=-10, vmax=10)
+    # Save to disk the outputs produced by any distribution makers
+    if ref_source == 'DistributionMaker':
+        outfile = os.path.join(args.outdir, args.ref_label + '.json.bz2')
+        ref.to_file(outfile)
+
+    if test_source == 'DistributionMaker':
+        outfile = os.path.join(args.outdir, args.test_label + '.json.bz2')
+        test.to_file(outfile)
+
+    if test.names != ref.names:
+        raise ValueError(
+            'Test map names %s do not match ref map names %s.'
+            % (test.names, ref.names)
+        )
+
+    plotter = Plotter(stamp='', outdir=args.outdir, fmt='pdf', log=False, annotate=False, symmetric=False, ratio=True)
+    for ref_map, test_map in zip(ref, test):
+        diff = test_map - ref_map
+        fract_diff = test_map / ref_map  - 1
+        logging.info('Map %s ..' % ref_map.name)
+        logging.info('  Totals:')
+        logging.info('    Ref :' + ('%.2f' % np.nansum(ref_map.nominal_values)).rjust(8))
+        logging.info('    Test:' + ('%.2f' % np.nansum(ref_map.nominal_values)).rjust(8))
+        logging.info('  Means:')
+        logging.info('    Ref :' + ('%.2f' % np.nanmean(ref_map.nominal_values))).rjust(8))
+        logging.info('    Test:' + ('%.2f' % np.nanmean(ref_map.nominal_values))).rjust(8))
+        logging.info('  Test - Ref, mean +/- std dev:')
+        logging.info('    %.4e +/- %.4e' + %(np.nanmean(diff.nominal_values), np.nanstd(diff.nominal_values)))
+        logging.info('  Test / Ref - 1, mean +/- std dev:')
+        logging.info('    %.4e +/- %.4e' + %(np.nanmean(fract_diff.nominal_values), np.nanstd(fract_diff.nominal_values)))
+        logging.info('')
+
+    plotter.plot_2d_array(ref, split_axis='pid', fname='%s_nominal' % args.ref_label)
+    plotter.plot_2d_array(test, split_axis='pid', fname='%s_nominal' % args.test_label)
+
+    plotter.label = '%s/%s - 1' % (args.test_label, args.ref_label)
+    plotter.plot_2d_array(test/ref-1., split_axis='pid', fname='fract_diff', cmap='seismic') #, vmin=-2,vmax=2)
+
+    plotter.label = '%s - %s' % (args.ref_label, args.test_label)
+    plotter.plot_2d_array(ref - test, split_axis='pid', fname='abs_diff', cmap='seismic') #, vmin=-10, vmax=10)
