524c9357c1cb8ed321f5cd7a8539c5e54d61ad2e
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-10-21 14:12:55 -0400

    feature-ful, stores output maps, stores summary statistics, command-line options incl. setting plot limits

--- pisa/scripts/compare.py
+++ pisa/scripts/compare.py
@@ -2,12 +2,12 @@
 # author: J.L. Lanfranchi
 # date:   October 8, 2016
 """
-Compares reco vbwkde vs. hist.
+Compare two PISA entities.
 """
 
 
-from argparse import ArgumentParser
-from collections import Iterable
+from argparse import ArgumentParser, ArgumentDefaultsHelpFormatter 
+from collections import Iterable, OrderedDict
 import os
 
 import numpy as np
@@ -15,7 +15,7 @@ from uncertainties import unumpy as unp
 
 from pisa.core.distribution_maker import DistributionMaker
 from pisa.core.map import Map, MapSet
-from pisa.utils.fileio import mkdir
+from pisa.utils.fileio import mkdir, to_file
 from pisa.utils.log import logging, set_verbosity
 from pisa.utils.tests import plot_cmp
 from pisa.utils.plotter import Plotter
@@ -25,10 +25,14 @@ if __name__ == '__main__':
     parser = ArgumentParser(
         description='''Compare two entities: Maps, map sets, pipelines, or
         distribution makers. One kind can be compared against another, so long
-        as the resulting map(s) have equivalent names and binning.'''
+        as the resulting map(s) have equivalent names and binning. At the end,
+        the result each specification is formatted into a MapSet and stored to
+        disk, so that e.g. re-running a DistributionMaker is unnecessary to
+        reproduce the results.''',
+        formatter_class=ArgumentDefaultsHelpFormatter
     )
     parser.add_argument(
-        '--outdir', metavar='DIR', type=str, default=None, required=True,
+        '--outdir', metavar='DIR', type=str, required=True,
         help='''Store output plots to this directory.'''
     )
     parser.add_argument(
@@ -36,6 +40,11 @@ if __name__ == '__main__':
         help='''Pipeline settings config file that generates reference
         output, or a stored map or map set. Repeat --ref option for multiple
         pipelines, maps, or map sets'''
+    )
+    parser.add_argument(
+        '--ref-abs', action='store_true',
+        help='''Use the absolute value of the reference plot for
+        comparisons.'''
     )
     parser.add_argument(
         '--ref-label', type=str, required=True,
@@ -54,6 +63,11 @@ if __name__ == '__main__':
         pipelines, maps, or map sets'''
     )
     parser.add_argument(
+        '--test-abs', action='store_true',
+        help='''Use the absolute value of the test plot for
+        comparisons.'''
+    )
+    parser.add_argument(
         '--test-label', type=str, required=True,
         help='''Label for test'''
     )
@@ -70,31 +84,75 @@ if __name__ == '__main__':
         quotes such that asterisks do not get expanded by the shell. Repeat the
         --combine option for multiple combine strings.'''
     )
-
-    grp = parser.add_mutually_exclusive_group(required=False)
-    grp.add_argument(
-        '--pdf', action='store_true',
-        help='''Save plots in PDF format.'''
-    )
-    grp.add_argument(
-        '--png', action='store_true',
-        help='''Save plots in PNG format.'''
-    )
-
     parser.add_argument(
-        '-v', action='count', default=None,
-        help='Set verbosity level'
+        '--pdf', action='store_true',
+        help='''Save plots in PDF format. If neither this nor --png is
+        specified, no plots are produced.'''
+    )
+    parser.add_argument(
+        '--png', action='store_true',
+        help='''Save plots in PNG format. If neither this nor --pdf is
+        specfied, no plots are produced.'''
+    )
+    parser.add_argument(
+        '--diff-vmin', type=float, required=False, default=None,
+        help='''Difference plot vmin; if you specify only one of --diff-vmin or
+        --diff-vmax, symmetric limits are automatically used (vmin = -vmax).'''
+    )
+    parser.add_argument(
+        '--diff-vmax', type=float, required=False, default=None,
+        help='''Difference plot vmax; if you specify only one of --diff-vmin or
+        --diff-vmax, symmetric limits are automatically used (vmin = -vmax).'''
+    )
+    parser.add_argument(
+        '--fract-diff-vmin', type=float, required=False, default=None,
+        help='''Fractional difference plot vmin; if you specify only one of
+        --fract-diff-vmin or --fract-diff-vmax, symmetric limits are
+        automatically used (vmin = -vmax).'''
+    )
+    parser.add_argument(
+        '--fract-diff-vmax', type=float, required=False, default=None,
+        help='''Fractional difference plot vmax; if you specify only one of
+        --fract-diff-vmin or --fract-diff-vmax, symmetric limits are
+        automatically used (vmin = -vmax).'''
+    )
+    parser.add_argument(
+        '-v', action='count',
+        help='Set verbosity level; repeat -v for higher level.'
     )
     args = parser.parse_args()
     set_verbosity(args.v)
 
-    #plot_formats = []
-    #if args.pdf:
-    #    plot_formats.append('pdf')
-    #if args.png:
-    #    plot_formats.append('png')
-    plt_fmt = 'pdf' if args.pdf else 'png'
+    ref_plot_label = args.ref_label
+    if args.ref_abs and not args.ref_label.startswith('abs'):
+        ref_plot_label = 'abs(%s)' % ref_plot_label
+    test_plot_label = args.test_label
+    if args.test_abs and not args.test_label.startswith('abs'):
+        test_plot_label = 'abs(%s)' % test_plot_label
 
+    plot_formats = []
+    if args.pdf:
+        plot_formats.append('pdf')
+    if args.png:
+        plot_formats.append('png')
+
+    diff_symm = True
+    if args.diff_vmin is not None and args.diff_vmax is None:
+        args.diff_vmax = -args.diff_vmin
+        diff_symm = False
+    if args.diff_vmax is not None and args.diff_vmin is None:
+        args.diff_vmin = -args.diff_vmax
+        diff_symm = False
+
+    fract_diff_symm = True
+    if args.fract_diff_vmin is not None and args.fract_diff_vmax is None:
+        args.fract_diff_vmax = -args.fract_diff_vmin
+        fract_diff_symm = False
+    if args.fract_diff_vmax is not None and args.fract_diff_vmin is None:
+        args.fract_diff_vmin = -args.fract_diff_vmax
+        fract_diff_symm = False
+
+    args.outdir = os.path.expanduser(os.path.expandvars(args.outdir))
     mkdir(args.outdir)
 
     # Get the reference distribution(s) into the form of a test MapSet
@@ -177,48 +235,142 @@ if __name__ == '__main__':
         ref = ref.combine_wildcard(args.combine)
         test = test.combine_wildcard(args.combine)
 
-    # Save to disk the outputs produced by any distribution makers
-    if ref_source == 'DistributionMaker':
-        outfile = os.path.join(args.outdir, args.ref_label + '.json.bz2')
-        ref.to_json(outfile)
+    # Save to disk the maps being plotted (excluding optional aboslute value
+    # operations)
+    ref.to_json(os.path.join(
+        args.outdir, 'maps__%s.json.bz2' % args.ref_label
+    ))
+    test.to_json(os.path.join(
+        args.outdir, 'maps__%s.json.bz2' % args.test_label
+    ))
 
-    if test_source == 'DistributionMaker':
-        outfile = os.path.join(args.outdir, args.test_label + '.json.bz2')
-        test.to_json(outfile)
-
-    if test.names != ref.names:
+    if set(test.names) != set(ref.names):
         raise ValueError(
             'Test map names %s do not match ref map names %s.'
-            % (test.names, ref.names)
+            % (sorted(test.names), sorted(ref.names))
         )
 
-    for ref_map, test_map in zip(ref, test):
-        diff = test_map - ref_map
-        fract_diff = test_map / ref_map  - 1
-        logging.info('Map %s ..' % ref_map.name)
+    reordered_test = []
+    new_ref = []
+    diff_maps = []
+    fract_diff_maps = []
+    summary_stats = {}
+    for ref_map in ref:
+        test_map = test[ref_map.name].reorder_dimensions(ref_map.binning)
+        if args.ref_abs:
+            ref_map = abs(ref_map)
+        if args.test_abs:
+            test_map = abs(test_map)
+
+        diff_map = test_map - ref_map
+        fract_diff_map = (test_map - ref_map)/ref_map
+
+        new_ref.append(ref_map)
+        reordered_test.append(test_map)
+        diff_maps.append(diff_map)
+        fract_diff_maps.append(fract_diff_map)
+
+        total_ref = np.sum(np.ma.masked_invalid(ref_map.nominal_values))
+        total_test = np.sum(np.ma.masked_invalid(test_map.nominal_values))
+
+        mean_ref = np.mean(np.ma.masked_invalid(ref_map.nominal_values))
+        mean_test = np.mean(np.ma.masked_invalid(test_map.nominal_values))
+
+        mean_diff = np.mean(np.ma.masked_invalid(diff_map.nominal_values))
+        std_diff = np.std(np.ma.masked_invalid(diff_map.nominal_values))
+        mean_fract_diff = np.mean(np.ma.masked_invalid(fract_diff_map.nominal_values))
+        std_fract_diff = np.std(np.ma.masked_invalid(fract_diff_map.nominal_values))
+
+        median_diff = np.median(np.ma.masked_invalid(diff_map.nominal_values))
+        mad_diff = np.median(np.abs(np.ma.masked_invalid(diff_map.nominal_values)))
+        median_fract_diff = np.median(np.ma.masked_invalid(fract_diff_map.nominal_values))
+        mad_fract_diff = np.median(np.abs(np.ma.masked_invalid(fract_diff_map.nominal_values)))
+
+        summary_stats[test_map.name] = OrderedDict([
+            ('total_ref', total_ref),
+            ('total_test', total_test),
+            ('mean_ref', mean_ref),
+            ('mean_test', mean_test),
+            ('mean_diff', mean_diff),
+            ('std_diff', std_diff),
+            ('mean_fract_diff', mean_fract_diff),
+            ('std_fract_diff', std_fract_diff),
+            ('median_diff', median_diff),
+            ('mad_diff', mad_diff),
+            ('median_fract_diff', median_fract_diff),
+            ('mad_fract_diff', mad_fract_diff),
+        ])
+
+        logging.info('Map %s...' % ref_map.name)
+        logging.info('  Pct Agreement: %+8.3f%s' % (100*mean_fract_diff, '%'))
         logging.info('  Totals:')
-        logging.info('    Ref :' + ('%.2f' % np.nansum(ref_map.nominal_values)).rjust(8))
-        logging.info('    Test:' + ('%.2f' % np.nansum(ref_map.nominal_values)).rjust(8))
+        logging.info('    Ref :' + ('%.2f' % total_ref).rjust(8))
+        logging.info('    Test:' + ('%.2f' % total_test).rjust(8))
         logging.info('  Means:')
-        logging.info('    Ref :' + ('%.2f' % np.nanmean(ref_map.nominal_values)).rjust(8))
-        logging.info('    Test:' + ('%.2f' % np.nanmean(ref_map.nominal_values)).rjust(8))
+        logging.info('    Ref :' + ('%.2f' % mean_ref).rjust(8))
+        logging.info('    Test:' + ('%.2f' % mean_test).rjust(8))
         logging.info('  Test - Ref, mean +/- std dev:')
-        logging.info('    %.4e +/- %.4e' %(np.nanmean(diff.nominal_values), np.nanstd(diff.nominal_values)))
-        logging.info('  Test / Ref - 1, mean +/- std dev:')
-        logging.info('    %.4e +/- %.4e' %(np.nanmean(fract_diff.nominal_values), np.nanstd(fract_diff.nominal_values)))
+        logging.info('    %.4e +/- %.4e' %(mean_diff, std_diff))
+        logging.info('  Test - Ref, median +/- median-abs-dev:')
+        logging.info('    %.4e +/- %.4e' %(median_diff, mad_diff))
+        logging.info('  (Test - Ref) / Ref, mean +/- std dev:')
+        logging.info('    %.4e +/- %.4e' %(mean_fract_diff, std_fract_diff))
+        logging.info('  (Test - Ref) / Ref, median +/- median-abs-dev:')
+        logging.info('    %.4e +/- %.4e' %(median_fract_diff, mad_fract_diff))
         logging.info('')
 
-    plotter = Plotter(stamp='', outdir=args.outdir, fmt=plt_fmt, log=False,
-                      annotate=False, symmetric=False, ratio=False)
-    plotter.plot_2d_array(ref, split_axis='pid', fname='%s_distr' % args.ref_label)
-    plotter.plot_2d_array(test, split_axis='pid', fname='%s_distr' % args.test_label)
+    ref = MapSet(new_ref)
+    test = MapSet(reordered_test)
+    diff = MapSet(diff_maps)
+    fract_diff = MapSet(fract_diff_maps)
 
-    plotter = Plotter(stamp='', outdir=args.outdir, fmt=plt_fmt, log=False,
-                      annotate=False, symmetric=True, ratio=True)
-    plotter.label = '%s/%s - 1' % (args.test_label, args.ref_label)
-    plotter.plot_2d_array(test/ref-1., split_axis='pid', fname='fract_diff', cmap='seismic') #, vmin=-2,vmax=2)
+    diff.to_json(os.path.join(
+        args.outdir,
+        'diff__%s__%s.json.bz2' %(test_plot_label, ref_plot_label)
+    ))
+    fract_diff.to_json(os.path.join(
+        args.outdir,
+        'fract_diff__%s___%s.json.bz2' %(test_plot_label, ref_plot_label)
+    ))
+    to_file(
+        summary_stats,
+        os.path.join(
+            args.outdir,
+            'stats__%s__%s.json.bz2' %(test_plot_label, ref_plot_label)
+        )
+    )
 
-    plotter = Plotter(stamp='', outdir=args.outdir, fmt=plt_fmt, log=False,
-                      annotate=False, symmetric=True, ratio=False)
-    plotter.label = '%s - %s' % (args.test_label, args.ref_label)
-    plotter.plot_2d_array(test - ref, split_axis='pid', fname='abs_diff', cmap='seismic') #, vmin=-10, vmax=10)
+    for plot_format in plot_formats:
+        plotter = Plotter(stamp='', outdir=args.outdir, fmt=plot_format,
+                          log=False, annotate=False,
+                          symmetric=False,
+                          ratio=False)
+        plotter.plot_2d_array(ref, split_axis='pid', fname='distr__%s'
+                              % ref_plot_label)
+        plotter.plot_2d_array(test, split_axis='pid', fname='distr__%s'
+                              % test_plot_label)
+
+        plotter = Plotter(stamp='', outdir=args.outdir, fmt=plot_format,
+                          log=False, annotate=False,
+                          symmetric=diff_symm,
+                          ratio=False)
+        plotter.label = '%s - %s' % (test_plot_label, ref_plot_label)
+        plotter.plot_2d_array(
+            test - ref, split_axis='pid',
+            fname='diff__%s__%s' % (test_plot_label, ref_plot_label),
+            cmap='seismic',
+            vmin=args.diff_vmin, vmax=args.diff_vmax
+        )
+
+        plotter = Plotter(stamp='', outdir=args.outdir, fmt=plot_format,
+                          log=False,
+                          annotate=False,
+                          symmetric=fract_diff_symm,
+                          ratio=True)
+        plotter.label = '%s/%s - 1' % (test_plot_label, ref_plot_label)
+        plotter.plot_2d_array(
+            test/ref - 1., split_axis='pid',
+            fname='fract_diff__%s__%s' % (test_plot_label, ref_plot_label),
+            cmap='seismic',
+            vmin=args.fract_diff_vmin, vmax=args.fract_diff_vmax
+        )
