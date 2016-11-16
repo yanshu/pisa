c87fcdb2b5f67db2f39fcb66778b53e9c6d7d0db
Philipp Eller <peller@phys.ethz.ch>
2016-11-16 13:36:49 -0500

    adding results files

---
+++ my_scripts/fit_sys.sh
@@ -0,0 +1,2 @@
+python pisa/utils/fit_discrete_sys.py -f pisa/resources/settings/discrete_sys_settings/nutau_holice_domeff_fits_mc.ini -t pisa/resources/settings/pipeline_settings/pipeline_settings_nutau_mc.ini --plot -sp bdt_cut=0.2
+python pisa/utils/fit_discrete_sys.py -f pisa/resources/settings/discrete_sys_settings/nutau_reco_fits_mc.ini -t pisa/resources/settings/pipeline_settings/pipeline_settings_nutau_mc.ini --plot -sp bdt_cut=0.2
