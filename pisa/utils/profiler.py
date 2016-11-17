9cddb04dbd95fb2e202b759cbec0b11a7ff39f2b
J.L. Lanfranchi <jll1062@phys.psu.edu>
2016-11-17 15:43:53 -0500

    remove circular import issues (hopefully)

--- pisa/utils/profiler.py
+++ pisa/utils/profiler.py
@@ -2,23 +2,23 @@ import time
 
 from line_profiler import LineProfiler
 
-from pisa.utils.log import logging, set_verbosity, tprofile
+import log
 
 
 __all__ = ['line_profile', 'profile']
 
 
-class log():
+class Log():
     """Class to redirect output into our logging stream."""
     def write(self, string):
         string = string.rstrip('\n')
         if string:
-            tprofile.debug(string)
+            log.tprofile.debug(string)
     def flush():
         pass
 
 
-tlog = log()
+tlog = Log()
 """Instance of a global timing logger"""
 
 line_profiler = LineProfiler()
@@ -48,49 +48,49 @@ def profile(func):
             return func(*args, **kwargs)
         finally:
             end_t = time.time()
-            tprofile.debug('module %s, function %s: %.4f ms'
-                           %(func.__module__, func.__name__,
-                             (end_t - start_t) * 1000))
+            log.tprofile.debug('module %s, function %s: %.4f ms'
+                               %(func.__module__, func.__name__,
+                                 (end_t - start_t) * 1000))
     return profiled_func
 
 
 def test_profile():
     @profile
     def get_number():
-        logging.debug('hello, i am get_number')
+        log.logging.debug('hello, i am get_number')
         for x in xrange(500000):
             yield x
 
     @profile
     def expensive_function():
-        logging.debug('hello, i am expensive fun')
+        log.logging.debug('hello, i am expensive fun')
         for x in get_number():
             i = x ^ x ^ x
         return 'some result!'
 
     result = expensive_function()
-    logging.info('<< PASSED : test_profile >>')
+    log.logging.info('<< PASSED : test_profile >>')
 
 
 def test_line_profile():
     @line_profile
     def get_number():
-        logging.debug('hello, i am get_number')
+        log.logging.debug('hello, i am get_number')
         for x in xrange(500000):
             yield x
 
     @line_profile
     def expensive_function():
-        logging.debug('hello, i am expensive fun')
+        log.logging.debug('hello, i am expensive fun')
         for x in get_number():
             i = x ^ x ^ x
         return 'some result!'
 
     result = expensive_function()
-    logging.info('<< PASSED : test_line_profile >>')
+    log.logging.info('<< PASSED : test_line_profile >>')
 
 
 if __name__ == '__main__':
-    set_verbosity(3)
+    log.set_verbosity(3)
     test_profile()
     test_line_profile()
